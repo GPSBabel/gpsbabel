6d30f81fbb3158cc584417c9d961f99891c654a9
robertl <robertl@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2002-08-16 22:46:41 +0000

    gpspilot: new. gpspilot, cetus: Reject files that don't seem to be ours.

--- trunk/gpsbabel/gpspilot.c
+++ trunk/gpsbabel/gpspilot.c
@@ -1,5 +1,5 @@
 /*
-    Read and write Cetus files.
+    Read and write GPSPilot Tracker files.
 
     Copyright (C) 2002 Robert Lipe, robertlipe@usa.net
 
@@ -23,34 +23,15 @@
 #include "coldsync/palm.h"
 #include "coldsync/pdb.h"
 
-#define MYNAME "Cetus"
-#define MYTYPE  0x43577074  	/* CWpt */
-#define MYCREATOR 0x63475053 	/* cGPS */
+#define MYNAME "GPSPilot"
+#define MYTYPE 0x706f696e  	/* poin */
+#define MYCREATOR 0x47704c69 	/* GpLi */
 
 struct record {
-	char type;
-	char ID[16]; /* Zero-terminated string. */
-	char name[61]; /* zero-terminated */
-	pdb_32 latitude; /* Big endian, degrees*1e7, s=negative */
-	pdb_32 longitude; /* same as lat; w=negative */
-	unsigned char hour;  /* sample time, UTC */
-	unsigned char min;
-	unsigned char sec;
-	unsigned char year; /* 2002 = 02 */
-	unsigned char mon;
-	unsigned char day;
-	pdb_32 elevation; /* Big endian, meters*100. blank=-1e8 */
-	char fix_quality; /* 3 = 3d fix, 0=no fix */
-	char sats_tracked; /* ff if averaged or unknown */
-	pdb_16 hdop;
-	pdb_16 dgpstime;
-	pdb_32 dgpsstn;
-	pdb_32 avgtime;
-	pdb_32 avgite;
-	pdb_16 icon;
-	char category;
-	char flagged;
-	char readonly;
+	pdb_32 longitude; 	/* Big endian, long * 3.6e6 */
+	pdb_32 latitude; 	/* similarly */
+	pdb_16 elevation; 	/* meters */
+	pdb_16 magvar; 		/* magnetic variation in degrees, neg = west */
 };
 
 static FILE *file_in;
@@ -102,11 +83,12 @@ data_read(void)
 	}
 
 	if ((pdb->creator != MYCREATOR) || (pdb->type != MYTYPE)) {
-		fatal(MYNAME ": Not a Cetus file.");
+		fatal(MYNAME ": Not a gpspilot file.");
 	}
 
 	for(pdb_rec = pdb->rec_index.rec; pdb_rec; pdb_rec=pdb_rec->next) {
 		waypoint *wpt_tmp;
+		char *vdata;
 
 		wpt_tmp = calloc(sizeof(*wpt_tmp),1);
 		if (wpt_tmp == NULL) {
@@ -114,25 +96,26 @@ data_read(void)
 		}
 
 		rec = (struct record *) pdb_rec->data;
-		wpt_tmp->shortname = strdup(rec->ID);
-		wpt_tmp->description = strdup(rec->name);
-		wpt_tmp->position.altitude.altitude_meters = pdb_read4(&rec->elevation) / 100.0;
+		wpt_tmp->position.longitude.degrees = pdb_read4(&rec->longitude) / 3.6e6; 
+		wpt_tmp->position.latitude.degrees = pdb_read4(&rec->latitude) / 3.6e6; 
+		wpt_tmp->position.altitude.altitude_meters = pdb_read2(&rec->elevation) / 100.0;
+	
+		vdata = pdb_rec->data + sizeof(*rec);
 
-		wpt_tmp->position.longitude.degrees = pdb_read4(&rec->longitude) / 10000000.0; 
-		wpt_tmp->position.latitude.degrees = pdb_read4(&rec->latitude) / 10000000.0; 
-		if (rec->year != 0xff) {
-			struct tm tm = {0};
-			time_t tval;
+		/*
+		 * This maping is a bit contrived.   
+		 * Name is up to 36. ID is up to 9.
+		 * Since 'ID' maps more clearly to "shortname" (and this
+		 * more likely to be resemble  a wayoint name in another
+		 * receiver) we use that for shortname and use 'name' as
+		 * our description.
+		 */
+		wpt_tmp->description = strdup(vdata);
+		vdata = vdata + strlen(vdata) + 1;
+
+		vdata = vdata + strlen(vdata) + 1;
+		wpt_tmp->shortname = strdup(vdata);
 		
-			tm.tm_min = rec->min;
-			tm.tm_hour = rec->hour;
-			tm.tm_mday = rec->day;
-			tm.tm_mon = rec->mon - 1;
-			tm.tm_year = rec->year + 100;
-
-			wpt_tmp->creation_time = mktime(&tm); 
-			
-		}
 		waypt_add(wpt_tmp);
 
 	} 
@@ -148,7 +131,7 @@ cetus_writewpt(waypoint *wpt)
 	struct tm *tm;
 
 	rec = calloc(sizeof(*rec),1);
-
+#if FIXME
 	strncpy(rec->ID, wpt->shortname, sizeof(rec->ID));
 	rec->ID[sizeof(rec->ID)-1] = 0;
 	strncpy(rec->name, wpt->description, sizeof(rec->name));
@@ -174,7 +157,7 @@ cetus_writewpt(waypoint *wpt)
 	pdb_write4(&rec->longitude, wpt->position.longitude.degrees * 10000000.0);
 	pdb_write4(&rec->latitude, wpt->position.latitude.degrees * 10000000.0);
 	pdb_write4(&rec->elevation, wpt->position.altitude.altitude_meters * 100.0);
-
+#endif
 	opdb_rec = new_Record (0, 0, ct++, sizeof(*rec), (const ubyte *)rec);
 
 	if (opdb_rec == NULL) {
@@ -186,21 +169,6 @@ cetus_writewpt(waypoint *wpt)
 	}
 }
 
-struct hdr{
-	char *wpt_name; 
-	waypoint *wpt;
-};
-
-static
-int 
-compare(const void *a, const void *b)
-{
-	const struct hdr *wa = a;
-	const struct hdr *wb = b;
-
-	return strcmp(wa->wpt->shortname, wb->wpt->shortname);
-}
-
 static void
 data_write(void)
 {
@@ -209,7 +177,7 @@ data_write(void)
         queue *elem, *tmp;
 	extern queue waypt_head;
         waypoint *waypointp;
-
+abort();
 	if (NULL == (opdb = new_pdb())) { 
 		fatal (MYNAME ": new_pdb failed\n");
 	}
@@ -218,38 +186,13 @@ data_write(void)
 	opdb->name[PDB_DBNAMELEN-1] = 0;
 	opdb->attributes = PDB_ATTR_BACKUP;
 	opdb->ctime = opdb->mtime = time(NULL) + 2082844800U;
-	opdb->type = MYTYPE;  /* CWpt */
-	opdb->creator = MYCREATOR; /* cGPS */
+	opdb->type = MYTYPE;  /* poin */
+	opdb->creator = MYCREATOR; /* GpLi */
 	opdb->version = 0;
-
-	/*
-	 * All this is to sort by waypoint names before going to Cetus.
-	 * Turns out plain old strcmp will do the trick...
-	 */
-
-	htable = malloc(ct * sizeof(*htable));
-	bh = htable;
-	if (!htable) {
-		fatal (MYNAME ":Cannot get array for sorting waypoints.");
-	}
-        QUEUE_FOR_EACH(&waypt_head, elem, tmp) {
-                waypointp = (waypoint *) elem;
-		bh->wpt = waypointp;
-		bh->wpt_name = waypointp->shortname;
-		bh ++;
-	}
-	qsort(htable, ct, sizeof(*bh), compare);
-
-	for (i=0;i<ct;i++) {
-		cetus_writewpt(htable[i].wpt);
-	}
-
-	pdb_Write(opdb, fileno(file_out));
-	free(htable);
 }
 
 
-ff_vecs_t cetus_vecs = {
+ff_vecs_t gpspilot_vecs = {
 	rd_init,
 	wr_init,
 	rd_deinit,
