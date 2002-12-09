bed48e675f75f27fa39047427d3e9f2b1eae6f91
nobody <nobody@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2002-12-09 06:00:27 +0000

    This commit was manufactured by cvs2svn to create tag 'gpsbabel_1_1_0_pre120902'.

---
+++ tags/gpsbabel_1_1_0_pre120902/gpsbabel/queue.h
@@ -0,0 +1,43 @@
+/*
+    Generic queueing utilities.
+
+    Copyright (C) 2002 Robert Lipe, robertlipe@usa.net
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111 USA
+
+ */
+
+typedef struct queue {
+	struct queue *next;
+	struct queue *prev;
+} queue;
+
+void enqueue(queue *new, queue *old);
+queue * dequeue(queue *element);
+
+#define QUEUE_INIT(head) (head)->next = (head)->prev = head
+#define QUEUE_FIRST(head) (head)->next
+#define QUEUE_NEXT(element) (element)->next
+#define QUEUE_LAST(head) (head)->prev
+#define QUEUE_EMPTY (head)->next = head
+
+#define ENQUEUE_TAIL(listhead, element) \
+		enqueue(element, (listhead)->prev)
+
+#define QUEUE_FOR_EACH(listhead, element, tmp) \
+	for ((element) = QUEUE_FIRST(listhead); \
+		(tmp) = QUEUE_NEXT(element), \
+		(element) != (listhead); \
+		(element) = (tmp))
