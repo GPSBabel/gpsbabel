5199e92849a48b71e40e2370c2d382c9c297738e
nobody <nobody@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2002-12-09 06:10:08 +0000

    This commit was manufactured by cvs2svn to create tag 'gpsbabel_1_1_0_pre120902a'.

---
+++ tags/gpsbabel_1_1_0_pre120902a/gpsbabel/gpx.c
@@ -0,0 +1,431 @@
+/*
+    Access GPX data files.
+
+    Copyright (C) 2002 Robert Lipe, robertlipe@usa.net
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111 USA
+
+ */
+
+#include "defs.h"
+#include <expat.h>
+
+static int in_wpt;
+static int in_rte;
+static int in_ele;
+static int in_name;
+static int in_time;
+static int in_desc;
+static int in_cdata;
+static int in_cmt;
+static int in_gs_type;
+static int in_gs_diff;
+static int in_gs_terr;
+static int in_gs_log;
+static char *cdatastr;
+
+static XML_Parser psr;
+
+static const char *gpx_version;
+static const char *gpx_creator;
+
+static waypoint *wpt_tmp;
+static FILE *fd;
+static FILE *ofd;
+
+#define MYNAME "GPX"
+#define MY_CBUF 4096
+
+static void
+tag_gpx(const char **attrv)
+{
+	const char **avp = &attrv[0];
+	while (*avp) {
+		if (strcmp(avp[0], "version") == 0) {
+			gpx_version = avp[1];
+		}
+		else if (strcmp(avp[0], "src") == 0) {
+			gpx_creator = avp[1];
+		}
+		avp+=2;
+	}
+}
+
+static void
+tag_wpt(const char **attrv)
+{
+	const char **avp = &attrv[0];
+
+	wpt_tmp = xcalloc(sizeof(*wpt_tmp), 1);
+
+	while (*avp) { 
+		if (strcmp(avp[0], "lat") == 0) {
+			sscanf(avp[1], "%lf", 
+				&wpt_tmp->position.latitude.degrees);
+		}
+		else if (strcmp(avp[0], "lon") == 0) {
+			sscanf(avp[1], "%lf", 
+				&wpt_tmp->position.longitude.degrees);
+		}
+		avp+=2;
+	}
+}
+
+static void
+gpx_start(void *data, const char *el, const char **attr)
+{
+
+	if (strcmp(el, "ele") == 0) {
+		in_ele++;
+	} if (strcmp(el, "name") == 0) {
+		in_name ++;
+	} if (strcmp(el, "gpx") == 0) {
+		tag_gpx(attr);
+	} if (strcmp(el, "wpt") == 0) {
+		in_wpt++;
+		tag_wpt(attr);
+	} if (strcmp(el, "desc") == 0) {
+		in_desc++;
+	} if (strcmp(el, "cmt") == 0) {
+		in_cmt++;
+	} if (strcmp(el, "rtept") == 0) {
+		in_rte++;
+		tag_wpt(attr);
+	} if (strcmp(el, "time") == 0) {
+		in_time++;
+	} if (strcmp(el, "groundspeak:type") == 0) {
+		in_gs_type++;
+	} if (strcmp(el, "groundspeak:difficulty") == 0) {
+		in_gs_diff++;
+	} if (strcmp(el, "groundspeak:terrain") == 0) {
+		in_gs_terr++;
+	} if (strcmp(el, "groundspeak:log") == 0) {
+		in_gs_log++;
+	}
+}
+
+struct
+gs_type_mapping{
+	geocache_type type;
+	const char *name;
+} gs_type_map[] = {
+	{ gt_traditional, "Traditional cache" },
+	{ gt_multi, "Multi-Cache" },
+	{ gt_virtual, "Virtual cache" }
+};
+static
+geocache_type
+gs_mktype(char *t)
+{
+	int i;
+	int sz = sizeof(gs_type_map) / sizeof(gs_type_map[0]);
+
+	for (i = 0; i < sz; i++) {
+		if (0 == strcmp(t, gs_type_map[i].name)) {
+			return gs_type_map[i].type;
+		}
+	}
+	return gt_unknown;
+}
+
+static void
+gpx_end(void *data, const char *el)
+{
+	float x;
+	if (in_cdata) {
+		if (in_name && in_wpt) {
+			wpt_tmp->shortname = xstrdup(cdatastr);
+		}
+		if (in_desc && in_wpt) {
+			wpt_tmp->notes = xstrdup(cdatastr);
+		}
+		if (in_cmt && in_wpt) {
+			wpt_tmp->description = xstrdup(cdatastr);
+		}
+		if (in_ele) {
+			sscanf(cdatastr, "%lf", 
+				&wpt_tmp->position.altitude.altitude_meters);
+		}
+		if (in_time && (in_wpt || in_rte)) {
+			struct tm tm;
+			sscanf(cdatastr, "%d-%d-%dT%d:%d:%dZ\n", 
+				&tm.tm_year,
+				&tm.tm_mon,
+				&tm.tm_mday,
+				&tm.tm_hour,
+				&tm.tm_min,
+				&tm.tm_sec);
+			tm.tm_mon -= 1;
+			tm.tm_year -= 1900;
+			tm.tm_isdst = 1;
+			wpt_tmp->creation_time = mktime(&tm);
+		}
+		if (in_wpt && in_gs_type && !in_gs_log) {
+			wpt_tmp->gc_data.type = gs_mktype(cdatastr);
+		}
+		if (in_wpt && in_gs_diff) {
+			sscanf(cdatastr, "%f", &x);
+			wpt_tmp->gc_data.diff = x * 10;
+		}
+		if (in_wpt && in_gs_terr) {
+			sscanf(cdatastr, "%f", &x);
+			wpt_tmp->gc_data.terr = x * 10;
+		}
+		in_cdata--;
+		memset(cdatastr, 0, MY_CBUF);
+	}
+	if (strcmp(el, "wpt") == 0) {
+		waypt_add(wpt_tmp);
+		in_wpt--;
+	}
+	else if (strcmp(el, "rtept") == 0) {
+/*		route_add(wpt_tmp); */
+		in_rte--;
+	} else if (strcmp(el, "name") == 0) {
+		in_name--;
+	} else if (strcmp(el, "desc") == 0) {
+		in_desc--;
+	} else if (strcmp(el, "cmt") == 0) {
+		in_cmt--;
+	} else if (strcmp(el, "ele") == 0) {
+		in_ele--;
+	} else if (strcmp(el, "time") == 0) {
+		in_time--;
+	} if (strcmp(el, "groundspeak:type") == 0) {
+		in_gs_type--;
+	} if (strcmp(el, "groundspeak:difficulty") == 0) {
+		in_gs_diff--;
+	} if (strcmp(el, "groundspeak:terrain") == 0) {
+		in_gs_terr--;
+	} if (strcmp(el, "groundspeak:log") == 0) {
+		in_gs_log--;
+	}
+}
+
+static void
+gpx_cdata(void *dta, const XML_Char *s, int len)
+{
+	char *estr;
+
+	/*
+	 * I'm exceedingly unamused that libexpat makes me keep all this
+	 * horrible state just I can concatenate buffers that it hands
+	 * me as a cdata that are fragmented becuae they span a read.  Grrr.
+	 */
+	if ((in_name && in_wpt) || (in_desc && in_wpt) || (in_ele) || 
+			(in_wpt && in_cmt) ||
+			(in_wpt && in_gs_type) || 
+			(in_wpt && in_gs_diff) || 
+			(in_wpt && in_gs_terr) || 
+			(in_time && (in_wpt || in_rte)))  {
+		estr = cdatastr + strlen(cdatastr);
+		memcpy(estr, s, len);
+		in_cdata++;
+	}
+}
+
+void
+gpx_rd_init(const char *fname, const char *args)
+{
+	fd = fopen(fname, "r");
+	if (fd == NULL) {
+		fatal(MYNAME ": Cannot open %s for reading\n", fname );
+	}
+	psr = XML_ParserCreate(NULL);
+	if (!psr) {
+		fatal(MYNAME ": Cannot create XML Parser\n");
+	}
+	cdatastr = xcalloc(MY_CBUF, 1);
+	XML_SetElementHandler(psr, gpx_start, gpx_end);
+	XML_SetCharacterDataHandler(psr, gpx_cdata);
+}
+
+static void
+gpx_rd_deinit(void)
+{
+	fclose(fd);
+}
+
+void
+gpx_wr_init(const char *fname, const char *args)
+{
+	ofd = fopen(fname, "w");
+	if (ofd == NULL) {
+		fatal(MYNAME ": open %s for writing\n", fname );
+	}
+}
+
+static void
+gpx_wr_deinit(void)
+{
+	fclose(ofd);
+}
+
+void
+gpx_read(void)
+{
+	int len;
+	int done = 0;
+	char buf[MY_CBUF];
+
+	while (!done) {
+		len = fread(buf, 1, sizeof(buf), fd);
+		done = feof(fd) || !len; 
+		if (!XML_Parse(psr, buf, len, done)) {
+			fatal(MYNAME ": XML parse error at %d: %s\n", 
+				XML_GetCurrentLineNumber(psr),
+				XML_ErrorString(XML_GetErrorCode(psr)));
+		}
+	}
+}
+
+/*
+ *
+ */
+static
+void
+gpx_write_time(const time_t timep)
+{
+	struct tm *tm = gmtime(&timep);
+	
+	fprintf(ofd, "<time>%02d-%02d-%02dT%02d:%02d:%02dZ</time>\n",
+		tm->tm_year+1900, 
+		tm->tm_mon+1, 
+		tm->tm_mday, 
+		tm->tm_hour, 
+		tm->tm_min, 
+		tm->tm_sec
+	);
+
+}
+
+static void
+gpx_waypt_pr(const waypoint *waypointp)
+{
+	const char *oname = global_opts.synthesize_shortnames ?
+				  mkshort(waypointp->description) : 
+				  waypointp->shortname;
+
+	fprintf(ofd, "<wpt lat=\"%lf\" lon=\"%lf\">\n",
+		waypointp->position.latitude.degrees,
+		waypointp->position.longitude.degrees);
+	if (oname) {
+		fprintf(ofd, "<name>%s</name>\n", oname);
+	}
+	if (waypointp->description) {
+		fprintf(ofd, "<cmt>");
+		fprintf(ofd, "<![CDATA[%s]]>", waypointp->description);
+		fprintf(ofd, "</cmt>\n");
+	}
+	if (waypointp->notes) {
+		fprintf(ofd, "<desc>");
+		fprintf(ofd, "<![CDATA[%s]]>", waypointp->notes);
+		fprintf(ofd, "</desc>\n");
+	} else {
+		if (waypointp->description) {
+			fprintf(ofd, "<desc>");
+			fprintf(ofd, "<![CDATA[%s]]>", waypointp->description);
+			fprintf(ofd, "</desc>\n");
+		}
+	}
+	if (waypointp->position.altitude.altitude_meters) {
+		fprintf(ofd, "<ele>\n%f\n</ele>\n",
+			 waypointp->position.altitude.altitude_meters);
+	}
+	if (waypointp->creation_time) {
+		gpx_write_time(waypointp->creation_time);
+	}
+	if (waypointp->url) {
+		fprintf(ofd, "<url>%s</url>\n", waypointp->url);
+	}
+	if (waypointp->url_link_text) {
+		fprintf(ofd, "<urlname>%s</urlname>\n",
+			 waypointp->url_link_text);
+	}
+	fprintf(ofd, "</wpt>\n");
+}
+
+static void
+gpx_track_hdr(const route_head *rte)
+{
+	fprintf(ofd, "<trk>\n");
+	if (rte->rte_name) {
+		fprintf(ofd, "  <name>\n");
+		fprintf(ofd, "  <![CDATA[%s]]>\n",rte->rte_name);
+		fprintf(ofd, "  </name>\n");
+	}
+	fprintf(ofd, "<trkseg>\n");
+}
+
+static void
+gpx_track_disp(const waypoint *waypointp)
+{
+	fprintf(ofd, "<trkpt lat=\"%lf\" lon=\"%lf\">\n",
+		waypointp->position.latitude.degrees,
+		waypointp->position.longitude.degrees);
+	if (waypointp->creation_time) {
+		gpx_write_time(waypointp->creation_time);
+	}
+	if (waypointp->position.altitude.altitude_meters != unknown_alt) {
+		fprintf(ofd, "<ele>\n%f\n</ele>\n",
+			 waypointp->position.altitude.altitude_meters);
+	}
+	fprintf(ofd, "</trkpt>\n");
+}
+
+static void
+gpx_track_tlr(const route_head *rte)
+{
+	fprintf(ofd, "</trkseg>\n");
+	fprintf(ofd, "</trk>\n");
+}
+
+static
+void gpx_track_pr()
+{
+	route_disp_all(gpx_track_hdr, gpx_track_tlr, gpx_track_disp);
+}
+
+void
+gpx_write(void)
+{
+	setshort_length(32);
+
+	fprintf(ofd, "<?xml version=\"1.0\"?>\n");
+	fprintf(ofd, "<gpx\n version=\"1.0\"\n");
+	fprintf(ofd, "creator=\"GPSBabel - http://gpsbabel.sourceforge.net\"\n");
+	fprintf(ofd, "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n");
+	fprintf(ofd, "xmlns=\"http://www.topografix.com/GPX/1/0\"\n");
+	fprintf(ofd, "xsi:schemaLocation=\"http://www.topografix.com/GPX/1/0 http://www.topografix.com/GPX/1/0/gpx.xsd\">\n");
+
+	switch(global_opts.objective) {
+		case trkdata: gpx_track_pr(); 
+		case wptdata: waypt_disp_all(gpx_waypt_pr);
+			      break;
+		default:
+			      break;
+	}
+
+	fprintf(ofd, "</gpx>\n");
+}
+
+ff_vecs_t gpx_vecs = {
+	gpx_rd_init,	
+	gpx_wr_init,	
+	gpx_rd_deinit,	
+	gpx_wr_deinit,	
+	gpx_read,
+	gpx_write,
+};
---
+++ tags/gpsbabel_1_1_0_pre120902a/gpsbabel/mapsend.c
@@ -0,0 +1,374 @@
+/*
+    Access Magellan Mapsend files.
+
+    Copyright (C) 2002 Robert Lipe, robertlipe@usa.net
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111 USA
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include "defs.h"
+#include "mapsend.h"
+
+static FILE *mapsend_file_in;
+static FILE *mapsend_file_out;
+
+static int endianness_tested;
+static int i_am_little_endian;
+
+#define MYNAME "mapsend"
+
+static void
+test_endianness(void)
+{
+	union {
+                long l;
+                unsigned char uc[sizeof (long)];
+        } u;
+
+        u.l = 1;
+        i_am_little_endian = u.uc[0];
+
+	endianness_tested = 1;
+
+}
+
+static void
+mapsend_rd_init(const char *fname, const char *args)
+{
+	mapsend_file_in = fopen(fname, "rb");
+	if (mapsend_file_in == NULL) {
+		fprintf(stderr, "Cannot open '%s' for reading\n", fname);
+		exit(1);
+	}
+}
+
+static void
+mapsend_rd_deinit(void)
+{
+	fclose(mapsend_file_in);
+}
+
+static 
+size_t 
+my_fread8(void *ptr, FILE *stream)
+{
+	char cbuf[8];
+	char *cptr = ptr;
+	size_t rv;
+
+	if (!endianness_tested) {
+		test_endianness();
+	}
+
+	if (i_am_little_endian) {	
+		rv = fread(ptr, 8, 1, stream);
+	} else { 
+		rv = fread(cbuf, 8, 1, stream);
+		cptr[0] = cbuf[7];
+		cptr[1] = cbuf[6];
+		cptr[2] = cbuf[5];
+		cptr[3] = cbuf[4];
+		cptr[4] = cbuf[3];
+		cptr[5] = cbuf[2];
+		cptr[6] = cbuf[1];
+		cptr[7] = cbuf[0];
+	}
+	return rv;
+}
+
+static 
+size_t
+my_fwrite8(void *ptr, FILE *stream)
+{
+	char cbuf[8];
+	char *cptr = ptr;
+
+	if (!endianness_tested) {
+		test_endianness();
+	}
+
+	if (i_am_little_endian) {	
+		return fwrite(ptr, 8, 1, stream);
+	} else {
+		cbuf[0] = cptr[7];
+		cbuf[1] = cptr[6];
+		cbuf[2] = cptr[5];
+		cbuf[3] = cptr[4];
+		cbuf[4] = cptr[3];
+		cbuf[5] = cptr[2];
+		cbuf[6] = cptr[1];
+		cbuf[7] = cptr[0];
+		return fwrite(cbuf, 8, 1, stream);
+	}
+}
+
+static 
+size_t 
+my_fread4(void *ptr, FILE *stream)
+{
+	char cbuf[4];
+	char *cptr = ptr;
+	size_t rv;
+	
+	if (!endianness_tested) {
+		test_endianness();
+	}
+
+	if (i_am_little_endian) {	
+		rv = fread(ptr, 4, 1, stream);
+	} else {
+		rv = fread(cbuf, 4, 1, stream);
+		cptr[0] = cbuf[3];
+		cptr[1] = cbuf[2];
+		cptr[2] = cbuf[1];
+		cptr[3] = cbuf[0];
+	}
+	return rv;
+}
+
+static 
+size_t
+my_fwrite4(void *ptr, FILE *stream)
+{
+	char cbuf[4];
+	char *cptr = ptr;
+
+	if (!endianness_tested) {
+		test_endianness();
+	}
+
+	if (i_am_little_endian) {	
+		return fwrite(ptr, 4, 1, stream);
+	} else {
+		cbuf[0] = cptr[3];
+		cbuf[1] = cptr[2];
+		cbuf[2] = cptr[1];
+		cbuf[3] = cptr[0];
+		return fwrite(cbuf, 4, 1, stream);
+	}
+}
+
+static void
+mapsend_wr_init(const char *fname, const char *args)
+{
+	mapsend_file_out = fopen(fname, "wb");
+	if (mapsend_file_out == NULL) {
+		fprintf(stderr, "Cannot open '%s' for writing\n", fname);
+		exit(1);
+	}
+}
+
+static void
+mapsend_wr_deinit(void)
+{
+	fclose(mapsend_file_out);
+}
+
+static void
+mapsend_wpt_read(void)
+{
+	char tbuf[256];
+	char name[257];
+	char comment[257];
+	char *p;
+	int wpt_count;
+	unsigned char scount;
+	int wpt_number;
+	char wpt_icon;
+	char wpt_status;
+	double wpt_alt;
+	double wpt_long;
+	double wpt_lat;
+	waypoint *wpt_tmp;
+	my_fread4(&wpt_count, mapsend_file_in);
+
+	while (wpt_count--) {
+		wpt_tmp = xcalloc(sizeof(*wpt_tmp), 1);
+
+		if (fread(&scount, sizeof(scount), 1, mapsend_file_in) < 1) {
+			fatal(MYNAME ": out of data reading %d waypoints\n",
+					wpt_count);
+		}
+		fread(tbuf, scount, 1, mapsend_file_in);
+		p = strncpy(name, tbuf, scount);
+		p[scount] = '\0';
+
+		fread(&scount, sizeof(scount), 1, mapsend_file_in);
+		fread(tbuf, scount, 1, mapsend_file_in);
+		p = strncpy(comment, tbuf, scount);
+		p[scount] = '\0';
+
+		my_fread4(&wpt_number, mapsend_file_in);
+		fread(&wpt_icon, sizeof(wpt_icon), 1, mapsend_file_in);
+		fread(&wpt_status, sizeof(wpt_status), 1, mapsend_file_in);
+		my_fread8(&wpt_alt, mapsend_file_in);
+		my_fread8(&wpt_long, mapsend_file_in);
+		my_fread8(&wpt_lat, mapsend_file_in);
+
+		wpt_tmp->shortname = xstrdup(name);
+		wpt_tmp->description = xstrdup(comment);
+		wpt_tmp->position.altitude.altitude_meters = wpt_alt;
+		wpt_tmp->position.latitude.degrees = -wpt_lat;
+		wpt_tmp->position.longitude.degrees = wpt_long;
+
+		waypt_add(wpt_tmp);
+	}
+}
+
+static void
+mapsend_track_read(void)
+{
+	char *trk_name;
+	unsigned char scount;
+	unsigned int trk_count;
+	double wpt_long;
+	double wpt_lat;
+	float wpt_alt;
+	int time;
+	int valid;
+	int centisecs;
+	route_head *track_head;
+	waypoint *wpt_tmp;
+
+	fread(&scount, sizeof(scount), 1, mapsend_file_in);
+	trk_name = xmalloc(scount + 1);
+	fread(trk_name, scount, 1, mapsend_file_in);
+	trk_name[scount] = '\0';
+	my_fread4(&trk_count, mapsend_file_in);
+
+	track_head = route_head_alloc();
+	track_head->rte_name = trk_name;
+	route_add_head(track_head);
+
+	while (trk_count--) {
+		my_fread8(&wpt_long, mapsend_file_in);
+		my_fread8(&wpt_lat, mapsend_file_in);
+		my_fread4(&wpt_alt, mapsend_file_in);
+		my_fread4(&time, mapsend_file_in);
+		my_fread4(&valid, mapsend_file_in);
+		fread(&centisecs, 1, 1, mapsend_file_in);
+
+		wpt_tmp = xcalloc(sizeof(*wpt_tmp), 1);
+		wpt_tmp->position.altitude.altitude_meters = wpt_alt;
+		wpt_tmp->position.latitude.degrees = -wpt_lat;
+		wpt_tmp->position.longitude.degrees = wpt_long;
+		wpt_tmp->creation_time = time;
+		route_add_wpt(track_head, wpt_tmp);
+	}
+}
+
+static void
+mapsend_read(void)
+{
+	mapsend_hdr hdr;
+	int type;
+
+	/*
+	 * Becuase of the silly struct packing and the goofy variable-length
+	 * strings, each member has to be read in one at a time.  Grrr.
+	 */
+
+	fread(&hdr, sizeof(hdr), 1, mapsend_file_in);
+	type = le_read16(&hdr.ms_type);
+
+	switch(type) {
+		case ms_type_wpt:
+			mapsend_wpt_read();
+			break;
+		case ms_type_track:
+			mapsend_track_read();
+			break;
+		case ms_type_log:
+			fatal(MYNAME ", GPS logs not supported.\n");
+		case ms_type_rgn:
+			fatal(MYNAME ", GPS regions not supported.\n");
+		default:
+			fatal(MYNAME ", unknown file type %d\n", type);
+	}
+}
+
+
+static void
+mapsend_waypt_pr(const waypoint *waypointp)
+{
+	int n;
+	unsigned char c;
+	double falt;
+	double flong;
+	double flat;
+	static int cnt = 0;
+	const char *sn = global_opts.synthesize_shortnames ? 
+		mkshort(waypointp->description) : waypointp->shortname;
+
+	c = strlen(sn);
+	fwrite(&c, 1, 1, mapsend_file_out);
+	fwrite(sn, c, 1, mapsend_file_out);
+
+	c = strlen(waypointp->description);
+	fwrite(&c, 1, 1, mapsend_file_out);
+	fwrite(waypointp->description, c, 1, mapsend_file_out);
+
+	/* #, icon, status */
+n = ++cnt;
+	my_fwrite4(&n, mapsend_file_out);
+n = 0;
+	fwrite(&n, 1, 1, mapsend_file_out);
+n = 1;
+	fwrite(&n, 1, 1, mapsend_file_out);
+
+	falt = waypointp->position.altitude.altitude_meters;
+	my_fwrite8(&falt, mapsend_file_out);
+
+	flong = waypointp->position.longitude.degrees;
+	my_fwrite8(&flong, mapsend_file_out);
+	flat = -waypointp->position.latitude.degrees;
+	my_fwrite8(&flat, mapsend_file_out);
+}
+
+void
+mapsend_wpt_write(void)
+{
+	mapsend_hdr hdr = {13, "4D533330 MS", "30", ms_type_wpt, 0};
+	int wpt_count = waypt_count();
+	int n = 0;
+
+	fwrite(&hdr, sizeof(hdr), 1, mapsend_file_out);
+	my_fwrite4(&wpt_count, mapsend_file_out);
+
+	waypt_disp_all(mapsend_waypt_pr);
+
+	my_fwrite4(&n, mapsend_file_out);
+/* TODO: Implement routes here */
+}
+
+#if LATER
+void 
+mapsend_trk_write(void)
+{
+	mapsend_hdr hdr = {13, "4D533334 MS", "34", ms_type_track, 0};
+}
+#endif
+
+ff_vecs_t mapsend_vecs = {
+	mapsend_rd_init,
+	mapsend_wr_init,
+	mapsend_rd_deinit,
+	mapsend_wr_deinit,
+	mapsend_read,
+	mapsend_wpt_write,
+};
