b9f9106d5bafe4c57344de2c5a1341e7d09123e1
robertl <robertl@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2003-01-06 23:41:47 +0000

    Cast ptr subtraction.  From Fouts.

--- gpsbabel/csv_util.c
+++ gpsbabel/csv_util.c
@@ -102,7 +102,7 @@ csv_stringtrim(const char *string, const char *enclosure)
 
     /* if we have enclosures, skip past them in pairs */
     if (elen) {
-	while (((p2 - p1) > elen) &&
+	while (((size_t) (p2 - p1) > elen) &&
 	       (strncmp(p1, enclosure, elen) == 0) &&
 	       (strncmp((p2 - elen + 1), enclosure, elen) == 0)) {
 	    p2 -= elen;
