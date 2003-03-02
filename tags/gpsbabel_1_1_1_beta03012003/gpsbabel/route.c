332268dcc368dc582d5b6afe138cc3f3cf27006f
nobody <nobody@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2003-03-02 03:14:01 +0000

    This commit was manufactured by cvs2svn to create tag 'gpsbabel_1_1_1_beta03012003'.

---
+++ tags/gpsbabel_1_1_1_beta03012003/gpsbabel/route.c
@@ -0,0 +1,113 @@
+/* 
+    Copyright (C) 2002 Robert Lipe, robertlipe@usa.net
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111 USA
+
+ */
+
+#include <stdio.h>
+#include "defs.h"
+
+static queue my_route_head;
+
+void
+route_init(void)
+{
+	QUEUE_INIT(&my_route_head);
+}
+
+
+route_head *
+route_head_alloc(void)
+{
+	route_head *rte_head;
+	rte_head = (route_head *) xcalloc(sizeof (*rte_head), 1);
+	QUEUE_INIT(&rte_head->Q);
+	return rte_head;
+}
+
+
+void
+route_add_head(route_head *rte)
+{
+	ENQUEUE_TAIL(&my_route_head, &rte->Q);
+	QUEUE_INIT(&rte->waypoint_list);
+}
+
+void
+route_add_wpt(route_head *rte, waypoint *wpt)
+{
+	ENQUEUE_TAIL(&rte->waypoint_list, &wpt->Q);
+}
+
+void
+route_free(route_head *rte)
+{
+	if ( rte->rte_name ) {
+		xfree(rte->rte_name);
+	}
+	if ( rte->rte_desc ) {
+		xfree(rte->rte_desc);
+	}
+	waypt_flush(&rte->waypoint_list);
+	xfree(rte);
+}
+
+void
+route_disp (const route_head *rh, waypt_cb cb )
+{
+	queue *elem, *tmp;
+	QUEUE_FOR_EACH(&rh->waypoint_list, elem, tmp) {
+		waypoint *waypointp;
+		waypointp = (waypoint *) elem;
+			(*cb)(waypointp);
+	}
+		
+}	
+void 
+route_disp_all(route_hdr rh, route_trl rt, waypt_cb wc)
+{
+	queue *elem, *tmp;
+	QUEUE_FOR_EACH(&my_route_head, elem, tmp) {
+		const route_head *rhp;
+		rhp = (route_head *) elem;
+		(*rh)(rhp);
+		route_disp(rhp, wc);
+		(*rt)(rhp);
+	}
+}
+void
+route_flush(queue *head)
+{
+	queue *elem, *tmp;
+	route_head *last = NULL;
+	
+	QUEUE_FOR_EACH(head, elem, tmp) {
+		if ( last ) {
+			route_free(last);
+		}
+		last = (route_head *)elem;
+	}
+	if ( last ) {
+		route_free(last);
+	}
+	QUEUE_INIT(head);
+}
+void
+route_flush_all()
+{
+	route_flush(&my_route_head);
+}
+
