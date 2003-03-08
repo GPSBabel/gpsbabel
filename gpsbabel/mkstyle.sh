5e5ef57d11ed0b1a6dae6d2daec590645a8eb093
robertl <robertl@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2003-03-08 22:51:32 +0000

    Add xmapwpt.style back in.   Exclude custom stye.   Thanx, Alex Mottram!

--- gpsbabel/mkstyle.sh
+++ gpsbabel/mkstyle.sh
@@ -3,6 +3,7 @@ for i in style/*.style
 do
 	A=`basename $i | sed "s/.style$//"`
 	[ $A = "README" ] && continue
+	[ $A = "custom.style" ] && continue
 	ALIST="{ \"$A\", $A } , $ALIST"
 	echo "static char $A[] = "
 	sed 's/\\/\\\\/;s/"/\\"/g;s/\(^.\)/"\1/g;s/\(.$\)/\1\\\n"/g' $i 
