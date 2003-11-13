4d34625f4b0e3250293f8922c4869d3252363608
robertl <robertl@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2003-11-13 05:28:33 +0000

    Move gpx_entitize to xml_entitize, call from geo.c in at least one of hte cases that we should be calling it (sigh) to correct a problem for Rick outputting URLs with '&' characters in them.

--- gpsbabel/defs.h
+++ gpsbabel/defs.h
@@ -338,6 +338,7 @@ char *strsub(char *s, char *search, char *replace);
 void rtrim(char *s);
 signed int get_tz_offset(void);
 const char *get_cache_icon(const waypoint *waypointp);
+char * xml_entitize(const char * str);
 
 /*
  * PalmOS records like fixed-point numbers, which should be rounded
