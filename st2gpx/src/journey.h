d008186824c24400e212c2909476d3c3899ee1cf
robertl <robertl@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2003-12-02 15:03:21 +0000

    This commit was generated by cvs2svn to compensate for changes in r564, which included commits to RCS files with non-trunk default branches.

---
+++ st2gpx/src/journey.h
@@ -0,0 +1,382 @@
+/*
+	journey.h
+
+	Extract data from MS Streets & Trips .est and Autoroute .axe files in GPX format.
+
+    Copyright (C) 2003 James Sherring, james_sherring@yahoo.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111 USA
+
+
+	This app depends on istorage & istorage-make from Pabs (pabs3@zip.to)
+	and James Clark's Expat xml parser from http://www.libexpat.org/.
+
+*/
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#define JOUR_FILE_HEAD_LEN 32
+#define JOUR_FILE_TAIL_LEN_EUR_V8 430
+#define JOUR_FILE_TAIL_LEN_USA_V8 426
+#define JOUR_FILE_TAIL_LEN_USA_V10 396
+
+// + text1 +2*text2
+#define JOUR_WPTREC_LEN 121
+
+#define JOUR_FILE_OS_NUMREC 16
+
+#define JOURWPT_REC_HEAD_LEN 41
+
+#define JOURWPT_RECOS_WPTNUM 0
+#define JOURWPT_RECOS_STR1LEN 40
+#define JOURWPT_RECOS_STR1 41
+// + val(JOURWPT_RECOS_STR1LEN)
+#define JOURWPT_RECOS_STR2LEN 44
+#define JOURWPT_RECOS_STR2 45
+
+// Still to test in Journey:
+// * points located by address Vs by mouse Vs by pushpin
+// * avoid areas
+// * drag route
+typedef struct f_jour_header
+{
+	int	iunkn0;
+	int	iunkn1;
+	int	iunkn2;
+	int	iunkn3;
+	unsigned int cpoints;
+	int	iunkn4;
+	int	iunkn5;
+	int	iunkn6;
+} tag_f_jour_header;
+
+typedef struct f_jour_pt_head
+{
+	// DB prim key for pushpin
+	// 0 if no pushpin
+	int UdId;
+	int	stop_dur_secs;
+	int	iunkn0;
+	int sched_arrive_flag;
+	// This may have more meaning,
+	// it changes with small changes in route.
+	// Perhaps just small changes in route time?
+	int	sched_depart_flag;
+	int	arrive_time_secs;
+	int	depart_time_secs;
+	int	iunkn1;
+	// The coords for the actual journey point, not neccessarily on a road.
+	//ie =lat/360*2^32
+	int scaled_lon;
+	int scaled_lat;
+	unsigned char cbtext1;
+} tag_f_jour_pt_head;
+
+// then jour_pt_head.cbtext1 bytes of text
+
+typedef struct f_jour_pt_mid
+{
+	// this just seems to be a const FF FE FF
+	char mid_tag[3];
+	unsigned char cbtext2;
+} tag_f_jour_pt_mid;
+
+// then 2*jour_pt_mid.cbtext2 bytes of wide text
+
+typedef struct f_jour_pt_tail
+{
+	// 0=fastest
+	// 1=shortest
+	// 2=prefered
+	// (from previous jour_pt)
+	int	route_rd_pref; // =0
+	// these seem const
+	int iunkn0; // = 1
+	int iunkn1; // = 4
+	int iunkn2; // =0
+	int iunkn3; // =1
+	int road_id;
+	double dist_along_rd_frac;
+	// The coords for the arrival point on the road
+	//ie =lat/360*2^32
+	int rd_arrive_sc_lat;
+	int rd_arrive_sc_lon;
+	// The coords for the departure point on the road
+	int rd_depart_sc_lat;
+	int rd_depart_sc_lon;
+	int	iunkn8;  // 4 
+	int	iunkn9;  // 1 
+	int	iunkn10; // 1 
+	int	iunkn11; // 6 
+	int	iunkn12; // 1
+	int	iunkn13; // 1
+	// ? size ?
+	int	iunkn14; // 30
+} tag_f_jour_pt_tail;
+
+typedef struct f_jour_opts
+{
+	// @00
+	short int	siunkn0;
+	float	funkn0;		
+	double	toll_rd_pref;
+	double	motorway_pref;	// ok
+	// @16
+	double	major_rd_pref; 
+	double	minor_rd_pref;
+	// @26
+	double	ferry_pref;
+	int		iunkn0;
+	// @32
+	int		iunkn1;
+	int		iunkn2;
+	double	fuel_price;			// ok
+	// @42
+	// 3 = per US Gallon
+	int		iunkn3;
+	int		fuel_price_unit;
+	int		iunkn4;
+	double	tank_capacity;		// ok
+	// @56
+	// 2 = liters
+	int		tank_capacity_units;
+	double	tank_start_level;	// ok
+	// @62
+	double	tank_warn_level;	// ok
+	int		iunkn5;	
+	double	fuel_use_city;		// ok
+	// @76
+	// 3 = liters/100k
+	int		fuel_use_city_unit;
+	int		iunkn6;
+	double	fuel_use_mway;		//ok
+	// @86
+	// 3 = liters/100k
+	int		fuel_use_mway_unit;
+	int		iunkn7;
+	double	mway_speed;			//ok
+	// @96
+	int		mway_speed_unit;
+	int		iunkn9;
+	double	lim_acc_speed;		//ok
+	// @a6
+	int		lim_acc_speed_unit;
+	int		iunkn11;
+	double	maj_rd_speed;		//ok
+	// @b6
+	int		maj_rd_speed_unit;
+	int		iunkn13;
+	double	min_rd_speed;		//ok
+	// @c6
+	int		min_rd_speed_unit;
+	int		iunkn15;
+	double	street_speed;		//ok
+	// @d6
+	int		street_speed_unit;
+	int		iunkn17;
+	int		iunkn18;
+	float	funkn1;
+	// @e6
+	int		iunkn19;
+	int		iunkn20;
+	int		iunkn21;
+	float	funkn2;
+	// @f6
+	int		iunkn22;
+	int		iunkn23;
+	int		iunkn24;
+	float	funkn3;
+	// @106
+	int		iunkn25;
+	int		iunkn26;
+	int		iunkn27;
+	float	funkn4;
+	// @116
+	int		iunkn28;
+	int		iunkn29;
+	int		iunkn30;
+	float	funkn5;
+	// @126
+	int		iunkn31;
+	int		iunkn32;
+	int		iunkn33;
+	int		start_drv_hr;		//ok
+	// @136
+	int		start_drv_min;		//ok
+	int		iunkn34;
+	int		end_drv_hr;			//ok
+	int		end_drv_min;		//ok
+	// @146
+	int		fuel_warn_flag;		
+	int		iunkn35;
+	int		fuel_fixed_rate_flag;
+	int		iunkn36;
+	// @156
+	int		fuel_cost_dist;	
+	int		route_flex_secs;	//ok
+	int		iunkn37;
+	int		rest_flag;
+	// @166
+	int		rest_dur_secs;
+	int		rest_interval_secs;
+} tag_jour_opts;
+
+typedef struct f_jour_opts_EUR_8
+{
+	int		iunkn38;
+	int		iunkn39;
+	// @176
+	int		iunkn40;
+	int		iunkn41;
+	int		iunkn42;
+	short int	siunkn1;
+	// @184
+	int		route_show_tm;
+	int		route_show_dist;
+	int		route_show_inst;
+	// @190
+	int		route_show_for;
+	int		route_show_to;
+	int		route_show_font_size;
+	int		iunkn43;
+	// @1a0
+	// not in usa version8
+	int		iunkn44;
+	unsigned short int count_avoid_regions;
+} tag_f_jour_opts_EUR_8;
+
+// Also works for USA9 and USA11
+typedef struct f_jour_opts_EUR_10
+{
+	int		iunkn38;
+	int		iunkn39;
+	// @176
+	int		iunkn40;
+	int		iunkn41;
+	int		iunkn42;
+
+	unsigned short int count_avoid_regions;
+} tag_f_jour_opts_EUR_10;
+
+typedef struct f_jour_opts_USA_8
+{
+	int		iunkn38;
+	int		iunkn39;
+	// @176
+	int		iunkn40;
+	int		iunkn41;
+	int		iunkn42;
+	short int	siunkn1;
+	// @184
+	int		iunkn43;
+	int		iunkn44;
+	int		iunkn45;
+	// @190
+	int		iunkn46;
+	int		iunkn47;
+	int		iunkn48;
+	int		iunkn49;
+	// @1a0
+	unsigned short int count_avoid_regions;
+} tag_f_jour_opts_USA_8;
+
+typedef struct f_jour_opts_USA_10
+{
+	int		iunkn38;
+	int		iunkn39;
+	// @176
+	int		iunkn40;
+	int		iunkn41;
+	int		iunkn42;
+	short int	siunkn1;
+	// @184
+	int		iunkn43;
+	int		iunkn44;
+	unsigned short int count_avoid_regions;
+} tag_f_jour_opts_USA_10;
+
+
+typedef struct f_jour_avoid
+{
+	float	x;
+	float	y;
+	float	z;
+	int		iunkn0;
+	int		iunkn1;
+	int		iunkn2;
+	int		iunkn3;
+	int		iunkn4;
+	int		iunkn5;
+	// avoid num? annot?
+	int		annot_num;
+} tag_f_jour_avoid;
+
+
+typedef struct f_jour_trailer
+{
+	int		iunkn0; // =0x65
+	int		iunkn1;
+} f_tag_jour_trailer;
+
+typedef struct jour_rtept
+{
+	int pthead_os;
+	// mem owned by this struct, as buf does not have terminating null
+	char* text1;
+	int ptmid_os;
+	// mem owned by this struct, as buf does not have terminating null
+	char* text2;
+	int pttail_os;
+	// pointer to pushpin owned by pushpinlist
+	struct pushpin* pushpin;
+	char garmin_ident[7];
+} tag_jour_trept;
+
+struct jour_rtept_rec * jour_rtept_rec_new();
+void jour_rtept_rec_delete(struct jour_rtept_rec * jourpt_rec);
+
+typedef struct journey
+{
+	int buf_len;
+	char* buf;
+	// We need to use nasty unsigned shorts if we compare to ->header->cpoints
+	int count_rtepts;
+	// a convenience for looking in multiple version opts 
+	int count_avoid_regions;
+	int header_os;
+	// an array of jour_rtept
+	struct jour_rtept * rtept_list;
+	int jopts_os;
+	int jopts_eur8_os;
+	int jopts_eur10_os;
+	int jopts_usa8_os;
+	int jopts_usa10_os;
+	// an array of ofsets to f_jour_avoid
+	int * avoid_os_list;
+	int trailer_os;
+} tag_journey;
+
+float scaled2deg(int scaled_deg);
+
+struct journey * journey_new();
+void journey_delete(struct journey * jour);
+
+struct journey * process_journey_stream (char* jour_in_file_name, struct pushpin_safelist * ppplist);
+
+
+#ifdef	__cplusplus
+}
+#endif
--- st2gpx/src/st2gpx.dsp
+++ st2gpx/src/st2gpx.dsp
@@ -39,9 +39,10 @@ RSC=rc.exe
 # PROP Use_Debug_Libraries 0
 # PROP Output_Dir "Release"
 # PROP Intermediate_Dir "Release"
+# PROP Ignore_Export_Lib 0
 # PROP Target_Dir ""
 # ADD BASE CPP /nologo /W3 /GX /O2 /D "WIN32" /D "NDEBUG" /D "_CONSOLE" /D "_MBCS" /YX /FD /c
-# ADD CPP /nologo /W3 /GX /O2 /D "WIN32" /D "NDEBUG" /D "_CONSOLE" /D "_MBCS" /YX /FD /c
+# ADD CPP /nologo /Zp1 /W3 /GX /O2 /D "WIN32" /D "NDEBUG" /D "_CONSOLE" /D "_MBCS" /YX /FD /c
 # ADD BASE RSC /l 0x409 /d "NDEBUG"
 # ADD RSC /l 0x409 /d "NDEBUG"
 BSC32=bscmake.exe
@@ -49,7 +50,7 @@ BSC32=bscmake.exe
 # ADD BSC32 /nologo
 LINK32=link.exe
 # ADD BASE LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /subsystem:console /machine:I386
-# ADD LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /subsystem:console /machine:I386
+# ADD LINK32 libexpat.lib kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /subsystem:console /machine:I386
 
 !ELSEIF  "$(CFG)" == "st2gpx - Win32 Debug"
 
@@ -62,9 +63,10 @@ LINK32=link.exe
 # PROP Use_Debug_Libraries 1
 # PROP Output_Dir "Debug"
 # PROP Intermediate_Dir "Debug"
+# PROP Ignore_Export_Lib 0
 # PROP Target_Dir ""
 # ADD BASE CPP /nologo /W3 /Gm /GX /ZI /Od /D "WIN32" /D "_DEBUG" /D "_CONSOLE" /D "_MBCS" /YX /FD /GZ /c
-# ADD CPP /nologo /W4 /Gm /GX /ZI /Od /D "WIN32" /D "_DEBUG" /D "_CONSOLE" /D "_MBCS" /FR /YX /FD /GZ /c
+# ADD CPP /nologo /Zp1 /Za /W3 /Gm /GX /ZI /Od /D "WIN32" /D "_DEBUG" /D "_CONSOLE" /D "_MBCS" /FR /YX /FD /GZ /c
 # ADD BASE RSC /l 0x409 /d "_DEBUG"
 # ADD RSC /l 0x409 /d "_DEBUG"
 BSC32=bscmake.exe
@@ -72,7 +74,8 @@ BSC32=bscmake.exe
 # ADD BSC32 /nologo
 LINK32=link.exe
 # ADD BASE LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /subsystem:console /debug /machine:I386 /pdbtype:sept
-# ADD LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /subsystem:console /debug /machine:I386 /pdbtype:sept
+# ADD LINK32 libexpat.lib kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /subsystem:console /debug /machine:I386 /pdbtype:sept
+# SUBTRACT LINK32 /profile /map
 
 !ENDIF 
 
@@ -85,7 +88,53 @@ LINK32=link.exe
 # PROP Default_Filter "cpp;c;cxx;rc;def;r;odl;idl;hpj;bat"
 # Begin Source File
 
+SOURCE=.\annotations.c
+# End Source File
+# Begin Source File
+
+SOURCE=.\contents.c
+# End Source File
+# Begin Source File
+
+SOURCE=.\debug.c
+# End Source File
+# Begin Source File
+
 SOURCE=.\getopt.c
+
+!IF  "$(CFG)" == "st2gpx - Win32 Release"
+
+!ELSEIF  "$(CFG)" == "st2gpx - Win32 Debug"
+
+# ADD CPP /Ze
+
+!ENDIF 
+
+# End Source File
+# Begin Source File
+
+SOURCE=.\journey.c
+# End Source File
+# Begin Source File
+
+SOURCE=.\nannol.c
+# End Source File
+# Begin Source File
+
+SOURCE=.\ppinutil.c
+# End Source File
+# Begin Source File
+
+SOURCE=.\properties.c
+
+!IF  "$(CFG)" == "st2gpx - Win32 Release"
+
+!ELSEIF  "$(CFG)" == "st2gpx - Win32 Debug"
+
+# ADD CPP /Ze
+
+!ENDIF 
+
 # End Source File
 # Begin Source File
 
@@ -95,10 +144,18 @@ SOURCE=.\pushpins.cpp
 
 !ELSEIF  "$(CFG)" == "st2gpx - Win32 Debug"
 
-# ADD CPP /W4
+# ADD CPP /Ze /W4 /Gi
 
 !ENDIF 
 
+# End Source File
+# Begin Source File
+
+SOURCE=.\readgpx.c
+# End Source File
+# Begin Source File
+
+SOURCE=.\readmpst.c
 # End Source File
 # Begin Source File
 
@@ -113,22 +170,74 @@ SOURCE=.\st2gpx.c
 !ENDIF 
 
 # End Source File
+# Begin Source File
+
+SOURCE=.\writegpx.c
+# End Source File
+# Begin Source File
+
+SOURCE=.\writepcx.c
+# End Source File
 # End Group
 # Begin Group "Header Files"
 
 # PROP Default_Filter "h;hpp;hxx;hm;inl"
 # Begin Source File
 
+SOURCE=.\annotations.h
+# End Source File
+# Begin Source File
+
+SOURCE=.\contents.h
+# End Source File
+# Begin Source File
+
 SOURCE=.\getopt.h
 # End Source File
 # Begin Source File
 
+SOURCE=.\gpx.h
+# End Source File
+# Begin Source File
+
+SOURCE=.\journey.h
+# End Source File
+# Begin Source File
+
+SOURCE=.\ppinutil.h
+# End Source File
+# Begin Source File
+
+SOURCE=.\properties.h
+# End Source File
+# Begin Source File
+
 SOURCE=.\pushpins.h
+# End Source File
+# Begin Source File
+
+SOURCE=.\st2gpx.h
 # End Source File
 # End Group
 # Begin Group "Resource Files"
 
 # PROP Default_Filter "ico;cur;bmp;dlg;rc2;rct;bin;rgs;gif;jpg;jpeg;jpe"
 # End Group
+# Begin Source File
+
+SOURCE=.\bugs.txt
+# End Source File
+# Begin Source File
+
+SOURCE=.\build.txt
+# End Source File
+# Begin Source File
+
+SOURCE=.\history.txt
+# End Source File
+# Begin Source File
+
+SOURCE=.\ToDo.txt
+# End Source File
 # End Target
 # End Project
