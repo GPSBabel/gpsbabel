a1a8f1ed86597433cd4b127f37677ca1fba88dc5
robertl <robertl@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2004-09-17 17:20:39 +0000

    Format/Round coords in dupe filter.

--- gpsbabel/duplicate.c
+++ gpsbabel/duplicate.c
@@ -197,8 +197,15 @@ duplicate_process(void)
 
 		if (lcopt) {
 			/* let sprintf take care of rounding */
-			sprintf(dupe.lat, "%11.6f", waypointp->latitude);
-			sprintf(dupe.lon, "%11.6f", waypointp->longitude);
+			sprintf(dupe.lat, "%11.4f", waypointp->latitude);
+			sprintf(dupe.lon, "%11.4f", waypointp->longitude);
+			/* The degrees2ddmm stuff is a feeble attempt to 
+			 * get everything rounded the same way in a precision
+			 * that's "close enough" for determining duplicates.
+			 */
+			sprintf(dupe.lat, "%11.3f", degrees2ddmm(waypointp->latitude));
+			sprintf(dupe.lon, "%11.3f", degrees2ddmm(waypointp->longitude));
+
 		}
 
 		crc = get_crc32(&dupe, sizeof(dupe));
