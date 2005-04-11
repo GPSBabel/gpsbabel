60bd33ebb724379105fd2d18384dc49f8dd6270f
robertl <robertl@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2005-04-11 20:30:57 +0000

    Use GPSBabel's abstraction for word size since the one from jeeps is dumb.

--- trunk/gpsbabel/jeeps/gpsport.h
+++ trunk/gpsbabel/jeeps/gpsport.h
@@ -2,15 +2,14 @@
  *  For portability any '32' type must be 32 bits
  *                  and '16' type must be 16 bits
  */
-typedef unsigned char UC;
-typedef short int16;
-typedef unsigned short uint16;
-typedef uint16 US;
 
-#if defined(__alpha)
-typedef int int32;
-typedef unsigned int uint32;
-#else
-typedef long int32;
-typedef unsigned long uint32;
-#endif
+/* Since GPSBabel already has an integer size abstraction layer and
+ * defs.h includes gbtypes.h before this file, just use that.
+ */
+
+typedef unsigned char UC;
+typedef uint16        US;
+typedef gbuint16      uint16;
+typedef gbint16       int16;
+typedef gbuint32      uint32;
+typedef gbint32       int32;
