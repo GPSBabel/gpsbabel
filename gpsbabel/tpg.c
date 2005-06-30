eef39ded9c45f591c899cf1f3ecc8dbe6eba592d
robertl <robertl@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2005-06-30 17:33:38 +0000

    Make Topo! not crash on waypoints that don't contain a description.

--- gpsbabel/tpg.c
+++ gpsbabel/tpg.c
@@ -185,7 +185,8 @@ tpg_read(void)
     	    
     	    stringsize = buff[0];
     	    
-    	    tpg_fread(&buff[0], stringsize, 1, tpg_file_in);
+    	    if (stringsize)
+		tpg_fread(&buff[0], stringsize, 1, tpg_file_in);
     	    buff[stringsize] = '\0';
     	    
     	    wpt_tmp->description = xstrdup(buff);
