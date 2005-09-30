d0567ba6c180dd46dfb2449a6d10d0f3b492ab96
oliskoli <oliskoli@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2005-09-30 23:14:22 +0000

    Sync changes with source tree; rel. 0.2

--- trunk/gpsbabel/win32/gui-2/about.pas
+++ trunk/gpsbabel/win32/gui-2/about.pas
@@ -23,29 +23,36 @@ interface
 uses
   gnugettextD4,
   Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
-  StdCtrls, Buttons, ExtCtrls,
+  StdCtrls, Buttons, ExtCtrls, ShellApi,
   common;
 
 type
   TfrmAbout = class(TForm)
     pnClient: TPanel;
-    Image1: TImage;
     pnBottom: TPanel;
-    BitBtn1: TBitBtn;
-    StaticText3: TStaticText;
-    Panel1: TPanel;
-    StaticText1: TStaticText;
-    StaticText2: TStaticText;
-    Image2: TImage;
-    StaticText4: TStaticText;
-    StaticText5: TStaticText;
-    StaticText6: TStaticText;
-    StaticText7: TStaticText;
-    StaticText8: TStaticText;
+    btnOK: TBitBtn;
+    pnCenter: TPanel;
+    stDescription: TStaticText;
+    imgBabelIcon: TImage;
+    stLicense: TStaticText;
+    lbHint1: TLabel;
+    lbVersion: TLabel;
+    lbxTranslators: TListBox;
+    lbTranslators: TLabel;
+    lbURL: TLabel;
+    lbCopyRight: TLabel;
+    lbMoreInfo: TLabel;
+    lbSFURL: TLabel;
     procedure FormCreate(Sender: TObject);
+    procedure lbURLMouseMove(Sender: TObject; Shift: TShiftState; X,
+      Y: Integer);
+    procedure pnCenterMouseMove(Sender: TObject; Shift: TShiftState; X,
+      Y: Integer);
+    procedure lbURLClick(Sender: TObject);
   private
     { Private declarations }
     FTitle: string;
+    FFlag: Integer;
   public
     { Public declarations }
   end;
@@ -57,16 +64,54 @@ implementation
 
 uses
   utils;
-  
+
 {$R *.DFM}
 
 procedure TfrmAbout.FormCreate(Sender: TObject);
 begin
   gnugettextD4.TranslateComponent(SELF);
   FTitle := Caption;
-  Caption := FTitle + ' GPSBabelGUI-2';
+  Caption := FTitle + ' ' + SGPSBabelTitle;
 
+  lbVersion.Caption := _('Version') + ' ' + SGPSBabelGUIVersion;
   FixStaticText(Self);
+
+  lbURL.Left := lbMoreInfo.Left + lbMoreInfo.Width + 4;
+  lbSFURL.Left := lbHint1.Left + lbHint1.Width + 4;
+  lbTranslators.Caption := lbTranslators.Caption + ':';
+
+  lbxTranslators.Items.Add(_('German by Olaf Klein'));
+  lbxTranslators.Items.Add(_('French by Lilian Morinon'));
+end;
+
+procedure TfrmAbout.lbURLMouseMove(Sender: TObject; Shift: TShiftState; X,
+  Y: Integer);
+begin
+  if (FFlag = 0) then
+  begin
+    FFlag := 1;
+    TLabel(Sender).Font.Color := clRed;
+  end;
+end;
+
+procedure TfrmAbout.pnCenterMouseMove(Sender: TObject; Shift: TShiftState; X,
+  Y: Integer);
+begin
+  if (FFlag = 1) then
+  begin
+    FFlag := 0;
+    lbURL.Font.Color := clBlue;
+    lbSFURL.Font.Color := clBlue;
+  end;
+end;
+
+procedure TfrmAbout.lbURLClick(Sender: TObject);
+var
+  url: string;
+begin
+  TLabel(Sender).Font.Color := clBlue;
+  url := 'http://' + TLabel(Sender).Caption;
+  ShellExecute(Self.Handle, 'open', PChar(url), nil, '', 0);
 end;
 
 end.
