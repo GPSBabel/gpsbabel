b66df95bfb6144003eb2f6f7cac5852b29199301
nobody <nobody@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2005-11-08 21:34:04 +0000

    This commit was manufactured by cvs2svn to create tag 'gpsbabel_1_2_8-beta11072005'.

---
+++ tags/gpsbabel_1_2_8-beta11072005/gpsbabel/fatal.c
@@ -0,0 +1,41 @@
+/*
+    Functions to indicate inconsistent or fatal conditions.
+
+    Copyright (C) 2002-2005 Robert Lipe, robertlipe@usa.net
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111 USA
+
+ */
+
+#include "defs.h"
+void
+fatal(const char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+void
+warning(const char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	va_end(ap);
+}
+
