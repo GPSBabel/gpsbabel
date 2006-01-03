2d6bafe5f0b3c7502b7e3a96de55eca3e9f3daab
robertl <robertl@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2006-01-03 15:50:02 +0000

    Rename uuid_generate to avoid conflich with one in darwin's libm.

--- trunk/gpsbabel/coastexp.c
+++ trunk/gpsbabel/coastexp.c
@@ -514,7 +514,7 @@ static char *
 ce_gen_uuid(void)
 {
 	uuid_t uu;
-	uuid_generate(uu);
+	gb_uuid_generate(uu);
 	sprintf(uuid_buffer, "%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x",
 		uu[0], uu[1], uu[2], uu[3], uu[4], uu[5], uu[6], uu[7],
 		uu[8], uu[9], uu[10], uu[11], uu[12], uu[13], uu[14], uu[15]);
