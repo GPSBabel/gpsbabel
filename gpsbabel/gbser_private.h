feb5425bbb57d95cb475606cccdd550dbff31f4c
robertl <robertl@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2006-07-13 21:25:24 +0000

    Pick up missing files from recent gbser commit.

---
+++ gpsbabel/gbser_private.h
@@ -0,0 +1,27 @@
+/*
+    Serial interface - private header for gbser*.c
+    
+    Copyright (C) 2006  Robert Lipe, robertlipe@usa.net
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111 USA
+
+ */
+
+#define MYMAGIC 0x91827364
+#define BUFSIZE 512
+
+void gbser__db(int l, const char *msg, ...);
+int gbser__fill_buffer(void *h, unsigned want, unsigned *ms);
+unsigned gbser__read_buffer(void *handle, void **buf, unsigned *len);
