dbe6c59c1075eaab0ef74b78b5c53715e4e46802
robertl <robertl@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2006-07-26 16:49:16 +0000

    Allow 200, not 199 in maggeo.

--- branches/gpsbabel_1_3_0_maint/gpsbabel/maggeo.c
+++ branches/gpsbabel_1_3_0_maint/gpsbabel/maggeo.c
@@ -55,7 +55,7 @@ maggeo_rd_deinit(void)
 static void
 maggeo_wr_init(const char *fname)
 {
-	if (waypt_count() >= 200) {
+	if (waypt_count() > 200) {
 		fatal(MYNAME ": eXplorist does not support more than 200 waypoints in one .gs file.\nDecrease the number of waypoints sent.\n");
 	}
 	maggeofile_out = xfopen(fname, "wb", MYNAME);
