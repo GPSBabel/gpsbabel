3830bf519de2a50b385f6784decd955375191f58
robertl <robertl@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2006-08-09 03:09:50 +0000

    Andy Armstrong adds support for version 4 of the WBT-200 (adds alt to tracks). Don't compute redundant course data in reader.

--- gpsbabel/wbt-200.c
+++ gpsbabel/wbt-200.c
@@ -1,21 +1,21 @@
 /*
  * Serial download of track data from a Wintec WBT-200.
- * 
+ *
  * Copyright (C) 2006 Andy Armstrong
- * 
- * This program is free software; you can redistribute it and/or modify it 
+ *
+ * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation; either version 2 of the License, or (at your
  * option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along 
- * with this program; if not, write to the Free Software Foundation, Inc., 
- * 59 Temple Place - Suite 330, Boston, MA 02111 USA 
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA 02111 USA
  */
 
 #include "defs.h"
@@ -23,8 +23,43 @@
 #include "grtcirc.h"
 #include <errno.h>
 
+#define MYNAME      "WBT-100/200"
+#define NL          "\x0D\x0A"
+
 #define BAUD        9600
 #define TIMEOUT     1500
+
+#define RECLEN_V1   12
+#define RECLEN_V2   16
+
+/* Used to sanity check data - from
+ *   http://hypertextbook.com/facts/2001/DanaWollman.shtml
+ * The MAXALT check doesn't need to be enabled unless there's
+ * a format with larger records than V2.
+ */
+/*#define MAXALT      120000*/
+
+#define _MAX(a, b) ((a) > (b) ? (a) : (b))
+#define RECLEN_MAX  _MAX(RECLEN_V1, RECLEN_V2)
+
+/* The formats here must be in ascending record length order so that
+ * each format identification attempt can read more data from the
+ * device if necessary. If that proves to be a bad order to try the
+ * heuristics the format matching code will have to be rejigged.
+ */
+static struct {
+    size_t      reclen;
+} fmt_version[] = {
+    {   RECLEN_V1   },
+    {   RECLEN_V2   },
+    {   0           }
+};
+
+/* Number of lines to skip while waiting for an ACK from a command. I've seen
+ * conversations with up to 30 lines of cruft before the response so 50 isn't
+ * too crazy.
+ */
+#define RETRIES     50
 
 /*
     A conversation looks like this
@@ -36,33 +71,48 @@
     >> $PFST,READLOGGER
     << $PFST,READLOGGER,*17
     << 0xFFFF, <length>, 0xFFFF
-    << (length + 1) * 12 bytes of data
+    << (length + 1) * 12 or 16 bytes of data
     << ====
     >> $PFST,NORMAL
     << $PFST,NORMAL,*02
 */
 
-/*static gpsdevh *fd;*/
 static void *fd;
 static FILE *fl;
 static char *port;
 static char *erase;
 
-#define MYNAME      "WBT-100/200"
-#define NL          "\x0D\x0A"
-
-struct read_state {
-	route_head	*route_head; 
-    double      plat, plon;     /* previous point */
-    time_t      ptim;
-    unsigned    wpn;
+struct buf_chunk {
+    struct buf_chunk    *next;
+    size_t              size;
+    size_t              used;
+    /* data follows in memory */
 };
 
-/* Number of lines to skip while waiting for an ACK from a command. I've seen
- * conversations with up to 30 lines of cruft before the response so 50 isn't
- * too crazy.
- */
-#define RETRIES     50
+#define buf_CHUNK_DATA(c) \
+    ((void *) ((struct buf_chunk *) (c) + 1))
+
+#define buf_CHUNK_PTR(c, offset) \
+    ((void *) ((char *) buf_CHUNK_DATA(c) + (offset)))
+
+struct buf_head {
+    struct buf_chunk    *head;
+    struct buf_chunk    *tail;
+    size_t              alloc;
+    size_t              used;
+    /* read position */
+    struct buf_chunk    *current;
+    unsigned long       offset;
+};
+
+struct read_state {
+	route_head	        *route_head;
+    double              plat, plon;     /* previous point */
+    time_t              ptim;
+    unsigned            wpn;
+
+    struct buf_head     data;
+};
 
 static void db(int l, const char *msg, ...) {
     va_list ap;
@@ -71,6 +121,99 @@ static void db(int l, const char *msg, ...) {
         vprintf(msg, ap);
     }
     va_end(ap);
+}
+
+/* Growable buffer support. TODO: Put this in a separate file and
+ * tidy up its interface.
+ */
+
+static void buf_init(struct buf_head *h, size_t alloc) {
+    h->head     = NULL;
+    h->tail     = NULL;
+    h->alloc    = alloc;
+    h->used     = 0;
+}
+
+static void buf_empty(struct buf_head *h) {
+    struct buf_chunk *chunk, *next;
+    for (chunk = h->head; chunk; chunk = next) {
+        next = chunk->next;
+        xfree(chunk);
+    }
+    h->head = NULL;
+    h->tail = NULL;
+    h->used = 0;
+}
+
+static void buf_rewind(struct buf_head *h) {
+    h->current = h->head;
+    h->offset  = 0;
+}
+
+static size_t buf_read(struct buf_head *h, void *data, size_t len) {
+    char *bp = data;
+
+    while (len != 0 && h->current != NULL) {
+        size_t avail = h->current->used - h->offset;
+        if (avail > len) { avail = len; }
+
+        memcpy(bp, buf_CHUNK_PTR(h->current, h->offset), avail);
+        h->offset   += avail;
+        bp          += avail;
+        len         -= avail;
+
+        if (h->offset == h->current->used) {
+            h->current  = h->current->next;
+            h->offset   = 0;
+        }
+    }
+
+    return bp - (char *) data;
+}
+
+static void buf_extend(struct buf_head *h, size_t amt) {
+    struct buf_chunk *c;
+    size_t sz = amt + sizeof(struct buf_chunk);
+    if (c = xmalloc(sz), NULL == c) {
+        fatal(MYNAME ": Can't allocate %lu bytes for buffer", (unsigned long) sz);
+    }
+
+    c->next = NULL;
+    c->size = amt;
+    c->used = 0;
+
+    if (NULL == h->head) {
+        h->head = c;
+    } else {
+        h->tail->next = c;
+    }
+
+    h->tail = c;
+}
+
+static void buf_write(struct buf_head *h, const void *data, size_t len) {
+    size_t avail;
+    const char *bp = data;
+
+    h->used += len;
+
+    if (NULL == h->tail) {
+        buf_extend(h, h->alloc);
+    }
+
+    for (;;) {
+        avail = h->tail->size - h->tail->used;
+        if (avail > len) { avail = len; }
+
+        memcpy((char *) buf_CHUNK_PTR(h->tail, h->tail->used), bp, avail);
+        h->tail->used   += avail;
+        bp              += avail;
+        len             -= avail;
+        if (len == 0) {
+            break;
+        }
+        buf_extend(h, h->alloc);
+    }
 }
 
 static void rd_drain() {
@@ -139,12 +282,11 @@ static int starts_with(const char *buf, const char *pat) {
 /* Send a command then wait for a line starting with the command string
  * to be returned.
  */
- 
 static void do_cmd(const char *cmd, const char *expect, char *buf, int len) {
     int try;
 
     rd_drain();
-    wr_cmd(cmd); 
+    wr_cmd(cmd);
     wr_cmd(NL);
 
     db(2, "Cmd: %s\n", cmd);
@@ -165,32 +307,69 @@ static void do_cmd(const char *cmd, const char *expect, char *buf, int len) {
     fatal(MYNAME ": Bad response from unit\n");
 }
 
+/* Issue a command that expects the same string to be echoed
+ * back as an ACK
+ */
 static void do_simple(const char *cmd, char *buf, int len) {
     do_cmd(cmd, cmd, buf, len);
 }
 
-static void data_chunk(struct read_state *st, const void *buf) {
-    char        wp_name[20];
-    gbuint32    tim;
-    double      lat, lon;
-    struct tm   t;
-    time_t      rtim;
-	waypoint	*wpt        = NULL;
-	const char *bp = buf;
-    
+/* Decompose binary date into discreet fields */
+#define _SPLIT_DATE(tim) \
+    int sec    = (((tim) >>  0) & 0x3F); \
+    int min    = (((tim) >>  6) & 0x3F); \
+    int hour   = (((tim) >> 12) & 0x1F); \
+    int mday   = (((tim) >> 17) & 0x1F); \
+    int mon    = (((tim) >> 22) & 0x0F); \
+    int year   = (((tim) >> 26) & 0x3F);
+
+static time_t decode_date(gbuint32 tim) {
+    _SPLIT_DATE(tim)
+    struct tm t;
+
+    t.tm_sec    = sec;
+    t.tm_min    = min;
+    t.tm_hour   = hour;
+    t.tm_mday   = mday;
+    t.tm_mon    = mon  -   1;
+    t.tm_year   = year + 100;
+
+    return mkgmtime(&t);
+}
+
+static int check_date(gbuint32 tim) {
+    _SPLIT_DATE(tim)
+
+    /* Sanity check the date. We don't allow years prior to 2004 because zero in
+    * those bits will usually indicate that we have an altitude instead of a
+    * date (i.e. that the data is the new format that uses 16 byte records).
+    */
+    return sec < 60 && min < 60 && hour < 24 &&
+            mday > 0 && mday <= 31 && mon > 0 && mon <= 12 && year >= 4;
+}
+
+static int data_chunk(struct read_state *st, const void *buf, int fmt) {
+    char       wp_name[20];
+    gbuint32   tim;
+    double     lat, lon, alt;
+    time_t     rtim;
+	waypoint   *wpt     = NULL;
+	const char *bp      = buf;
+    size_t     buf_used = fmt_version[fmt].reclen;
+
     tim = le_read32(bp + 0);
-    
+
     lat = (double) ((gbint32) le_read32(bp + 4)) / 10000000;
     lon = (double) ((gbint32) le_read32(bp + 8)) / 10000000;
-    
-    t.tm_sec    = ((tim >>  0) & 0x3F);
-    t.tm_min    = ((tim >>  6) & 0x3F);
-    t.tm_hour   = ((tim >> 12) & 0x1F);
-    t.tm_mday   = ((tim >> 17) & 0x1F);
-    t.tm_mon    = ((tim >> 22) & 0x0F) - 1;
-    t.tm_year   = ((tim >> 26) & 0x3F) + 100;
-    
-    rtim = mkgmtime(&t);
+
+    /* Handle extra fields in longer records here. */
+    if (buf_used >= 16) {
+        alt = (double) le_read32(bp + 12) / 10;
+    } else {
+        alt = unknown_alt;
+    }
+
+    rtim = decode_date(tim);
 
     if (lat >= 100) {
         /* Start new track in the northern hemisphere */
@@ -202,29 +381,21 @@ static void data_chunk(struct read_state *st, const void *buf) {
         lat += 100;
         st->route_head = NULL;
     } else {
-        double speed, gcd, dtim, rtm;
+        /* TODO: Should this code execute for /every/ waypoint - even the first in
+         * a track? Presumably it should because the first point looks as valid as
+         * any other.
+         */
+
 		wpt = waypt_new();
-	
+
 		wpt->latitude	    = lat;;
 		wpt->longitude	    = lon;
+		wpt->altitude       = alt;
 		wpt->creation_time  = rtim;
 		wpt->centiseconds   = 0;
-	
+
 		sprintf(wp_name, "WP%04d", ++st->wpn);
 		wpt->shortname      = xstrdup(wp_name);
-		
-		/* Broken down to make it easier to find the source of rounding errors */
-        gcd                 = gcdist(RAD(st->plat), RAD(st->plon), RAD(lat), RAD(lon));
-        gcd                 = (double) ((long) (gcd * 1000000 + 0.5)) / 1000000;
-        dtim                = rtim - st->ptim;
-        rtm                 = radtometers(gcd);
-        speed               = rtm / dtim;
-        
-        wpt->speed          = speed;
-		wpt->course         = heading_true_degrees(RAD(st->plat), RAD(st->plon),
-	                                      RAD(lat), RAD(lon));
-		wpt->pdop	        = 0;
-		wpt->fix 		    = fix_unknown;
 
 		if (NULL == st->route_head)	{
 		    db(1, "New Track\n");
@@ -234,24 +405,127 @@ static void data_chunk(struct read_state *st, const void *buf) {
 
 		track_add_wpt(st->route_head, wpt);
     }
-	
+
 	st->ptim = rtim;
 	st->plat = lat;
 	st->plon = lon;
+
+    return 1;
+}
+
+/* Return true iff the data appears valid with the specified record length */
+static int is_valid(struct buf_head *h, int fmt) {
+    char buf[RECLEN_MAX];
+    size_t reclen = fmt_version[fmt].reclen;
+
+    buf_rewind(h);
+
+    db(2, "Checking %lu bytes of data against format %d\n", h->used, fmt);
+
+    for (;;) {
+        size_t got = buf_read(h, buf, reclen);
+        gbuint32 tim;
+        /* Don't mind odd bytes at the end - we may
+         * be examining an incomplete dataset.
+         */
+        if (got != reclen) {
+            break;
+        }
+
+        tim = le_read32(buf + 0);
+        if (!check_date(tim)) {
+            return 0;
+        }
+
+        if (reclen > 12) {
+#ifdef MAXALT
+            gbuint32 alt = le_read32(buf + 12);
+            if (alt > MAXALT * 10) {
+                return 0;
+            }
+#endif
+        }
+    }
+
+    return 1;
+}
+
+static void process_data(struct read_state *pst, int fmt) {
+    char buf[RECLEN_MAX];
+    size_t reclen = fmt_version[fmt].reclen;
+
+    buf_rewind(&pst->data);
+
+    db(2, "Processing %lu bytes of data using format %d\n", pst->data.used, fmt);
+
+    for (;;) {
+        size_t got = buf_read(&pst->data, buf, reclen);
+        if (got != reclen) {
+            break;
+        }
+        data_chunk(pst, buf, fmt);
+    }
+}
+
+static void state_init(struct read_state *pst) {
+    pst->route_head = NULL;
+    pst->wpn        = 0;
+
+    buf_init(&pst->data, RECLEN_V1 * RECLEN_V2);
+}
+
+static void state_empty(struct read_state *pst) {
+    buf_empty(&pst->data);
+    state_init(pst);
 }
 
 static void file_read(void) {
-    char                buf[12];
-    int                 rc;
+    char                buf[512];
+    size_t              rc;
     struct read_state   st;
-    
-    st.route_head = NULL;
-    st.wpn = 0;
-    
-    rc = fread(buf, sizeof(buf), 1, fl);
-    while (rc == 1) {
-        data_chunk(&st, buf);
-        rc = fread(buf, sizeof(buf), 1, fl);
+    int                 fmt;
+
+    state_init(&st);
+
+    /* Read the whole file into the buffer */
+    rc = fread(buf, 1, sizeof(buf), fl);
+    while (rc != 0) {
+        buf_write(&st.data, buf, rc);
+        rc = fread(buf, 1, sizeof(buf), fl);
+    }
+
+    if (!feof(fl)) {
+        fatal(MYNAME ": Read error");
+    }
+
+    /* Try to guess the data format */
+    for (fmt = 0; fmt_version[fmt].reclen != 0; fmt++) {
+        size_t reclen = fmt_version[fmt].reclen;
+        if ((st.data.used % reclen) == 0 && is_valid(&st.data, fmt)) {
+            break;
+        }
+    }
+
+    if (fmt_version[fmt].reclen == 0) {
+        fatal(MYNAME ": Can't autodetect data format");
+    }
+
+    process_data(&st, fmt);
+
+    state_empty(&st);
+}
+
+static void want_bytes(struct buf_head *h, size_t len) {
+    char buf[512];
+
+    db(3, "Reading %lu bytes from device\n", (unsigned long) len);
+
+    while (len > 0) {
+        size_t want = sizeof(buf);
+        if (want > len) { want = len; }
+        rd_buf(buf, want);
+        buf_write(h, buf, want);
+        len -= want;
     }
 }
 
@@ -261,31 +535,64 @@ static void data_read(void) {
      * long lines returning only the first N characters
      */
     char                line_buf[100];
-    int                 count, d;
+    int                 fmt;
+    unsigned long       count;
     struct read_state   st;
-    
-    st.route_head = NULL;
-    st.wpn = 0;
 
+    state_init(&st);
+
+    /* We could potentially parse the version string to find out which
+     * data format to use - but it's not clear how the version string
+     * will increment in the future - so just now it's more future-
+     * proof to rely on analysing the data. We need to be able to do
+     * that with files anyway - because they're not versioned.
+     */
     do_simple("$PFST,FIRMWAREVERSION", line_buf, sizeof(line_buf));
+
     do_simple("$PFST,NORMAL",          line_buf, sizeof(line_buf));
     do_simple("$PFST,READLOGGER",      line_buf, sizeof(line_buf));
-    
+
     /* Now we're into binary mode */
     rd_buf(line_buf, 6);            /* six byte header */
     count = le_read16(line_buf + 2) + 1;
     if (count == 0x10000) {
         count = 0;
     }
-    
-    db(1, "Reading %d data\n", count);
-    for (d = 0; d < count; d++) {
-        rd_buf(line_buf, 12);       /* twelve byte record */
-        data_chunk(&st, line_buf);
+
+    db(3, "%lu points available\n", count);
+
+    /* Loop through the known formats requesting more data from the
+     * device each time. When the device contains only a single
+     * point the first format will get a false positive - so we'll
+     * lose the altitude data.
+     */
+    for (fmt = 0; fmt_version[fmt].reclen != 0; fmt++) {
+        size_t reclen = fmt_version[fmt].reclen;
+        size_t want   = reclen * count;
+
+        if (want < st.data.used) {
+            fatal(MYNAME ": Internal error: formats not ordered in ascending size order");
+        }
+
+        db(3, "Want %lu bytes of data\n", (unsigned long) want);
+
+        /* Top up the buffer */
+        want_bytes(&st.data, want - st.data.used);
+
+        /* And see if it's valid */
+        if (is_valid(&st.data, fmt)) {
+            break;
+        }
     }
-    
+
+    if (fmt_version[fmt].reclen == 0) {
+        fatal(MYNAME ": Can't autodetect data format");
+    }
+
+    process_data(&st, fmt);
+
     /* Erase data? */
-    
+
     if (*erase != '0') {
         int f;
         db(1, "Erasing data\n");
@@ -301,11 +608,12 @@ static void data_read(void) {
     }
 
     do_simple("$PFST,NORMAL", line_buf, sizeof(line_buf));
-    
+
+    state_empty(&st);
 }
 
 static arglist_t wbt_sargs[] = {
-    { "erase", &erase, "Erase device data after download", 
+    { "erase", &erase, "Erase device data after download",
         "0", ARGTYPE_BOOL, ARG_NOMINMAX },
     ARG_TERMINATOR
 };
