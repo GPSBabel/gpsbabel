934eae56a6ff25e4a8c284dc6e41e8b258d04d60
oliskoli <oliskoli@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2006-10-04 21:17:19 +0000

    Change my (Olaf's) reference e-mail address.

--- trunk/gpsbabel/win32/gui-2/filter.pas
+++ trunk/gpsbabel/win32/gui-2/filter.pas
@@ -1,7 +1,7 @@
 unit filter;
 
 {
-    Copyright (C) 2005 Olaf Klein, o.b.klein@t-online.de
+    Copyright (C) 2005 Olaf Klein, o.b.klein@gpsbabel.org
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
