3ce0da418bc1df1dde2b998b8e02d98cd71eb14b
oliskoli <oliskoli@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2007-04-21 19:42:31 +0000

    tef_xml: Add support for "TourExchangeFormat" version 1.5. Try to repair cutted descriptions. Regenerate gpx reference.

--- trunk/gpsbabel/tef_xml.c
+++ trunk/gpsbabel/tef_xml.c
@@ -26,17 +26,18 @@
 #include "defs.h"
 #include "xmlgeneric.h"
 
-static waypoint *wpt_tmp = NULL;
-static int item_count = -1;
-static int waypoints = 0;
-
+static waypoint *wpt_tmp;
+static int item_count;
+static int waypoints;
+static double version;
 static route_head *route = NULL;
 
 static char *routevia = NULL;
 
 static arglist_t tef_xml_args[] = 
 {
-	{"routevia", &routevia, "Include only via stations in route", NULL, ARGTYPE_BOOL, ARG_NOMINMAX},
+	{"routevia", &routevia, "Include only via stations in route", 
+		NULL, ARGTYPE_BOOL, ARG_NOMINMAX},
 	ARG_TERMINATOR
 };
 
@@ -56,19 +57,32 @@ tef_xml_read(void)
 
 #else
 
+static char *
+trimmed_strdup(const char *str)
+{
+	char *c1, *c2, *res;
+	
+	c1 = xstrdup(str);
+	c2 = lrtrim(c1);
+	if (*c2) res = xstrdup(c2);
+	else res = NULL;
+	xfree(c1);
+	return res;
+}
+
 static xg_callback	tef_start, tef_header, tef_list_start, tef_list_end;
 static xg_callback	tef_item_start, tef_point, tef_item_end;
 
 static 
 xg_tag_mapping tef_xml_map[] = {
 	{ tef_start,		cb_start,	"/TEF" },
-	{ tef_header,   	cb_start,	"/TEF/Header" },
-	{ tef_list_start,   	cb_start,	"/TEF/WaypointList" },
-	{ tef_item_start, 	cb_start, 	"/TEF/WaypointList/Item" },
-	{ tef_point, 		cb_start, 	"/TEF/WaypointList/Item/Point" },
-	{ tef_item_end, 	cb_end, 	"/TEF/WaypointList/Item" },
-	{ tef_list_end, 	cb_end, 	"/TEF/WaypointList" },
-	{ NULL, 		0, 		NULL }
+	{ tef_header,		cb_start,	"/TEF/Header" },
+	{ tef_list_start,	cb_start,	"/TEF/WaypointList" },
+	{ tef_item_start,	cb_start,	"/TEF/WaypointList/Item" },
+	{ tef_point,		cb_start,	"/TEF/WaypointList/Item/Point" },
+	{ tef_item_end,		cb_end,		"/TEF/WaypointList/Item" },
+	{ tef_list_end,		cb_end,		"/TEF/WaypointList" },
+	{ NULL,			0,		NULL }
 };
 
 
@@ -81,192 +95,200 @@ tef_start(const char *args, const char **attrv)
 {
 	int valid = 0;
 	const char **avp = &attrv[0];
-        while (*avp) 
-	{
-    	    if (strcmp(avp[0], "Comment") == 0) 
-	    {
-		if (0 == strcmp(avp[1], "TourExchangeFormat")) valid = 1;
-	    }
-    	    avp+=2;
-        }
-	if (!valid)
-	{
-	    fatal(MYNAME ": error in source file.\n");
+
+        while (*avp) {
+		if (0 == case_ignore_strcmp(avp[0], "Comment")) {
+			if (0 == case_ignore_strcmp(avp[1], "TourExchangeFormat"))
+				valid = 1;
+		}
+		else if (0 == case_ignore_strcmp(avp[0], "Version"))
+			version = atof(avp[1]);
+		avp+=2;
 	}
+	if (!valid)
+		fatal(MYNAME ": Error in source file.\n");
 }
 
 /*
  * tef_header: "Name" > Route name, "Software" > Route descr.
  */
 
-void 
+static void 
 tef_header(const char *args, const char **attrv)
 {
 	const char **avp = &attrv[0];
 
 	route = route_head_alloc();
-	while (*avp) 
-	{
-	    if (strcmp(avp[0], "Name") == 0) 
-	    {
-		route->rte_name = xstrdup(avp[1]);
-	    }
-	    else if (strcmp(avp[0], "Software") == 0) 
-	    {
-		route->rte_desc = xstrdup(avp[1]);
-	    }
-	    avp+=2;
+	while (*avp) {
+		if (case_ignore_strcmp(avp[0], "Name") == 0)
+			route->rte_name = trimmed_strdup(avp[1]);
+		else if (case_ignore_strcmp(avp[0], "Software") == 0)
+			route->rte_desc = trimmed_strdup(avp[1]);
+		avp+=2;
 	}
 	route_add_head(route);
 }
 
-/*
- *
- */
- 
-void
+static void
 tef_list_start(const char *args, const char **attrv)
 {
 	const char **avp = &attrv[0];
 
-        while (*avp) 
-	{
-    	    if (strcmp(avp[0], "ItemCount") == 0) 
-	    {
-		sscanf(avp[1], "%d", &item_count);
-	    }
-            avp+=2;
-        }
+        while (*avp) {
+		if (strcmp(avp[0], "ItemCount") == 0)
+			sscanf(avp[1], "%d", &item_count);
+		avp+=2;
+	}
 }
 
-/*
- * local procedure for waypoint handling
+/* in "TourExchangeFormat" the following can happen:
+ *
+ * SegDescription="L34\Wittlicher Strasse"
+ * PointDescription="Wittlicher Strasse (  "
+ *
+ * fix_notes tries to create a new PointDescription, which
+ * should be "Wittlicher Strasse (L34)" for the example above
  */
- 
-void waypoint_final()
+
+static char *
+fix_notes(const char *name, char *notes)
+{
+	char *cleft, *cright, *cback, *ctmp;
+	
+	if ((! name) || (! notes)) return notes;
+	
+	/* do we have a BACKSLASH in shortname ? */
+	cback = strchr(name, '\\');
+	if ((! cback) || (cback == name)) return notes;
+
+	/* do we have left, but no right parenthesis in notes ? */
+	if (! (cleft = strchr(notes, '('))) return notes;
+	cright = strchr(notes, ')');
+	if (cright && (cright > cleft)) return notes;
+	
+	/* now contruct the new name */
+	ctmp = lrtrim(xstrndup(notes, cleft - notes));
+	xfree(notes);
+	xasprintf(&notes, "%s (%*.*s)", ctmp, cback - name, cback - name, name);
+	xfree(ctmp);
+
+	return notes;
+}
+
+static void
+waypoint_final()
 {
 	int via;
 	if (wpt_tmp == NULL) return;
 
 	via = wpt_tmp->microseconds;
 	wpt_tmp->microseconds = 0;
-	
-	if (via != 0)
-	    waypt_add(wpt_tmp);
-	    
-	if (route != NULL)
-	{
-	    if ((via != 0) || (routevia == NULL))
-	    {
-		waypoint *wpt = waypt_dupe(wpt_tmp);
-		route_add_wpt(route, wpt);
-	    }
+
+	if (version < 2) {	/* keep the old behaviour */
+		wpt_tmp->notes = wpt_tmp->description;
+		wpt_tmp->description = NULL;
 	}
-	
+
+	wpt_tmp->notes = fix_notes(wpt_tmp->shortname, wpt_tmp->notes);
+
+	if (via != 0)
+		waypt_add(wpt_tmp);
+
+	if (route != NULL) {
+		if ((via != 0) || (routevia == NULL)) {
+			waypoint *wpt = waypt_dupe(wpt_tmp);
+			route_add_wpt(route, wpt);
+		}
+	}
+
 	if (via == 0)
-	    waypt_free(wpt_tmp);
-	    
+		waypt_free(wpt_tmp);
+
 	wpt_tmp = NULL;
 }
 
-void 
+static void 
 tef_item_end(const char *args, const char **unused)
 {
 	waypoint_final();
 }
 
-/*
- * 
- */
- 
-void 
+static void 
 tef_list_end(const char *args, const char **unused)
 {
 	waypoint_final();
 	if (waypoints != item_count)
-	{
-	    fatal(MYNAME ": waypoint count differs to internal \"ItemCount\"! (%d to %d)\n", waypoints, item_count);
-	}
+		fatal(MYNAME ": waypoint count differs to internal \"ItemCount\"! (%d to %d)\n", 
+			waypoints, item_count);
 }
 
-/*
- * 
- */
- 
-void 
+static void 
 tef_item_start(const char *args, const char **attrv)
 {
 	const char **avp = &attrv[0];
 
-	wpt_tmp = waypt_new();
-	
-	if ((waypoints == 1) || (waypoints == item_count)) 
-	    wpt_tmp->microseconds++;
-	    
 	waypoints++;
-	
+
+	wpt_tmp = waypt_new();
+	if ((waypoints == 1) || (waypoints == item_count))
+		wpt_tmp->microseconds++;
+
 	while (*avp) 
 	{
-	    if (0 == strcmp(avp[0], "PointDescription")) 
-	    {
-		wpt_tmp->shortname = xstrdup(avp[1]);
-	    }
-	    if (0 == strcmp(avp[0], "SegDescription")) 
-	    {
-		wpt_tmp->description = xstrdup(avp[1]);
-	    }
-	    if ((0 == strcmp(avp[0], "ViaStation")) && (0 == strcmp(avp[1], "true")))
-	    {
-		wpt_tmp->microseconds = 1;
-	    }
-	    avp+=2;
+		if (0 == case_ignore_strcmp(avp[0], "SegDescription"))
+			wpt_tmp->shortname = trimmed_strdup(avp[1]);
+		else if (0 == case_ignore_strcmp(avp[0], "PointDescription"))
+			wpt_tmp->description = trimmed_strdup(avp[1]);
+		else if ((0 == case_ignore_strcmp(avp[0], "ViaStation")) && 
+			 (0 == case_ignore_strcmp(avp[1], "true")))
+			wpt_tmp->microseconds = 1;		/* only a flag */
+
+		/* new in TEF V2 */
+		else if (0 == case_ignore_strcmp(avp[0], "Instruction"))
+			wpt_tmp->description = trimmed_strdup(avp[1]);
+		else if (0 == case_ignore_strcmp(avp[0], "Altitude"))
+			wpt_tmp->altitude = atof(avp[1]);
+		else if (0 == case_ignore_strcmp(avp[0], "TimeStamp"))
+			/* nothing for the moment */;
+
+		avp+=2;
 	}
 }
 
-/*
- * 
- */
- 
-void 
+static void 
 tef_point(const char *args, const char **attrv)
 {
-        const char **avp = &attrv[0];
+	const char **avp = &attrv[0];
 	char *comma;
 
 	if (!wpt_tmp) return;
 	
-        while (*avp) 
-	{
-    	    if (strcmp(avp[0], "y") == 0) 
-	    {
-		comma = strstr(avp[1], ",");
-		if (comma) *comma='.';
-                sscanf(avp[1], "%lf", &wpt_tmp->latitude);
-	    }
-	    else if (strcmp(avp[0], "x") == 0) 
-	    {
-		comma = strstr(avp[1], ",");
-		if (comma) *comma='.';
-		sscanf(avp[1], "%lf", &wpt_tmp->longitude);
-	    }
-            avp+=2;
-        }
+	while (*avp) {
+		if (strcmp(avp[0], "y") == 0) {
+			comma = strstr(avp[1], ",");
+			if (comma) *comma='.';
+			sscanf(avp[1], "%lf", &wpt_tmp->latitude);
+		}
+		else if (strcmp(avp[0], "x") == 0) {
+			comma = strstr(avp[1], ",");
+			if (comma) *comma='.';
+			sscanf(avp[1], "%lf", &wpt_tmp->longitude);
+		}
+		avp+=2;
+	}
 }
 
-/*
- * 
- */
- 
 static void 
 tef_xml_rd_init(const char *fname)
 {
+	wpt_tmp = NULL;
+	waypoints = 0;
+	item_count = -1;
+	version = 1.5;
+
 	xml_init(fname, tef_xml_map, NULL);
 }
 
-/*
- * 
- */
- 
 static void 
 tef_xml_read(void)
 {
@@ -275,11 +297,7 @@ tef_xml_read(void)
 
 #endif
 
-/*
- * 
- */
- 
-void 
+static void 
 tef_xml_rd_deinit(void)
 {
 	xml_deinit();
