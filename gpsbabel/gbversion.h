d3a70f5a83bec70d927dfdbcbfb5db74a58c3326
robertl <robertl@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2008-02-18 04:37:25 +0000

    Bump version (really this time!) for beta.

--- gpsbabel/gbversion.h
+++ gpsbabel/gbversion.h
@@ -4,5 +4,5 @@
  *
  * Isn't simplification via automation grand?
  */
-#define VERSION "1.3.5-beta20080214"
+#define VERSION "1.3.5-beta20080217"
 #define WEB_DOC_DIR "http://www.gpsbabel.org/htmldoc-1.3.5"
