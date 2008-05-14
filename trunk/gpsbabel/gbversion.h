87c20696b07129ee44ca483546a77616c239644e
robertl <robertl@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2008-05-14 15:54:55 +0000

    Move m241 to its own type instead of a barnacle on mtk.

--- trunk/gpsbabel/gbversion.h
+++ trunk/gpsbabel/gbversion.h
@@ -4,5 +4,5 @@
  *
  * Isn't simplification via automation grand?
  */
-#define VERSION "1.3.5"
-#define WEB_DOC_DIR "http://www.gpsbabel.org/htmldoc-1.3.5"
+#define VERSION "1.3.5-beta20080514"
+#define WEB_DOC_DIR "http://www.gpsbabel.org/htmldoc-development"
