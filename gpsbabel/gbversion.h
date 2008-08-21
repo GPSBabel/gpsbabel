257f443fd0071613d14e0db7088403006ad33ab8
oliskoli <oliskoli@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2008-08-21 20:11:23 +0000

    Bump to a right beta version.

--- gpsbabel/gbversion.h
+++ gpsbabel/gbversion.h
@@ -4,5 +4,5 @@
  *
  * Isn't simplification via automation grand?
  */
-#define VERSION "1.3.5-beta20080811"
+#define VERSION "1.3.6-beta20080821"
 #define WEB_DOC_DIR "http://www.gpsbabel.org/htmldoc-development"
