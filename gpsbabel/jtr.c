df470b81f98c050da069651713415547bf40e459
oliskoli <oliskoli@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2008-09-22 19:37:27 +0000

    jtr: Remove unwanted test code.

--- gpsbabel/jtr.c
+++ gpsbabel/jtr.c
@@ -165,7 +165,6 @@ jtr_read(void)
 		if (avltree_find(trkpts, buf, NULL)) continue;
 
 		wpt = waypt_new();
-		avltree_insert(trkpts, buf, wpt);
 
 		wpt->latitude = lat;
 		wpt->longitude = lon;
@@ -190,6 +189,7 @@ jtr_read(void)
 			track_add_head(trk);
 		}
 
+		avltree_insert(trkpts, buf, wpt);
 		track_add_wpt(trk, wpt);
 	}
 }
@@ -209,7 +209,7 @@ jtr_wr_deinit(void)
 static void
 jtr_trkpt_disp_cb(const waypoint *wpt)
 {
-	char *str;
+	char *str, *tmp;
 	char stime[10], sdate[7], scourse[6], sspeed[8];
 	struct tm tm;
 
@@ -243,8 +243,13 @@ jtr_trkpt_disp_cb(const waypoint *wpt)
 		scourse,
 		sdate);
 
-	xasprintf2(&str, "%s*%02X", str, nmea_cksum(str));
-	xasprintf2(&str, "%s,,,E,,E*%02X\r", str, nmea_cksum(str));
+	xasprintf(&tmp, "%s*%02X", str, nmea_cksum(str));
+	xfree(str);
+	str = tmp;
+
+	xasprintf(&tmp, "%s,,,E,,E*%02X\r", str, nmea_cksum(str));
+	xfree(str);
+	str = tmp;
 
 	gbfputs(str, fout);
 	xfree(str);
