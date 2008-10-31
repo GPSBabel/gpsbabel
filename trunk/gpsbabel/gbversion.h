3c9fccb4d62f85bd0aa16febf6bcd392f82206f4
robertl <robertl@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2008-10-31 12:43:21 +0000

    Bump version numbers for 1.3.6 release.

--- trunk/gpsbabel/gbversion.h
+++ trunk/gpsbabel/gbversion.h
@@ -4,5 +4,5 @@
  *
  * Isn't simplification via automation grand?
  */
-#define VERSION "1.3.6-beta20081006"
-#define WEB_DOC_DIR "http://www.gpsbabel.org/htmldoc-development"
+#define VERSION "1.3.6"
+#define WEB_DOC_DIR "http://www.gpsbabel.org/htmldoc-1.3.6"
