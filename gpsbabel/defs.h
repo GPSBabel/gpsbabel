fb401254c7bad8d4d8c0eec7a04edb3b00054642
robertl <robertl@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2009-02-04 23:40:49 +0000

    Bring in the KML writer from Google Earth 5.0.  Set the extents of the  time sliders automatically.

--- gpsbabel/defs.h
+++ gpsbabel/defs.h
@@ -542,6 +542,8 @@ void waypt_init_bounds(bounds *bounds);
 int waypt_bounds_valid(bounds *bounds);
 void waypt_add_to_bounds(bounds *bounds, const waypoint *waypointp);
 void waypt_compute_bounds(bounds *);
+double gcgeodist(const double lat1, const double lon1,
+                 const double lat2, const double lon2);
 void waypt_flush(queue *);
 void waypt_flush_all(void);
 unsigned int waypt_count(void);
