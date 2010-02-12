7446aadad101fdbe557ad5912fda9396f6f2698c
robertl <robertl@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2010-02-12 15:24:39 +0000

    VPL improvements.  Decode HDOP and VDOP.

--- gpsbabel/vpl.c
+++ gpsbabel/vpl.c
@@ -30,7 +30,7 @@
 	sentence.  For example:
 
 75700241FA59FB242CD500CF041984991E0B0A0C09060613064509060613064424824D68FF00000800051C000271
-00--1111111122222222333344445555--66----777777777777--------------------------------------CC
+00--1111111122222222333344445555--667788999999999999AAAAAAAAAAAA--------------------------XX
 
 0 - Sentence type
 1 - Latitude in hex (signed, divide by 0xE1000 for decimal degrees)
@@ -45,9 +45,59 @@
 	8499 -> 33945 * (360/65535) = 186.47
 6 - Number of sats
 	0B -> 11
-7 - Date and Time (YYMMDDHHMMSS)
+7 - HDOP (divide by 8)
+	0A -> 10 / 8 = 1.25
+8 - VDOP (divide by 8)
+	0C -> 12 / 8 = 1.5
+9 - Date and Time (YYMMDDHHMMSS)
 	090606130645 = June 6, 2009 13:06:45
-C - Checksum (xor, ala NMEA)
+A - Previous line date and time (?)
+	090606130644 = June 6, 2009 13:06:44
+X - Checksum (xor, ala NMEA)
+
+***********************************
+* Unused, but (at least partially) decoded sentences
+
+******
+0D - Yaw Gyro (This field is not currently decoded herein)
+	This field is written 25 times per second.  It contains the raw values
+	from the yaw gyro.  Positive values for right turns, negative values
+	for left turns.
+
+0D00FEFA09
+00??1111CC
+
+0 - Sentence Type
+? - An unknown field with observed values between 0 and 3.
+1 - Yaw Gyro value
+C - Checksum
+
+******
+31 - Distance Traveled
+	This field is written once a second.  It contains the number of
+	meters traveled since the navigation system was last turned on.
+
+310000117050
+0011111111CC
+
+0 - Sentence Type
+1 - Distance Traveled in Meters
+C - Checksum
+
+******
+35 - Raw Position
+	This field is written 5 times per second.  It contains Latitude
+	and Longitude, as well as two currently unknown angular values.
+
+35CFBB5CBC1744CB1BD9023308C2
+00111111112222222233334444CC
+
+0 - Sentence Type
+1 - Longitude (divide by 0x8CA000 for decimal degrees)
+2 - Latitude (divide by 0x8CA000 for decimal degrees)
+3 - Unknown angular value (multiply by 360/65535)
+4 - Unknown angular value (multiply by 360/65535)
+C - Checksum
 
 */
 
@@ -100,11 +150,9 @@ vpl_read(void)
 	}
 
 	while((ibuf = gbfgetstr(vpl_file_in))) {
-		if(strncmp(ibuf, "75", 2)) {
-			// Only bother with '75' sentences
-			continue;
-		}
-		vpl_parse_75_sentence(ibuf);
+		if(strncmp(ibuf, "75", 2) == 0) {
+			vpl_parse_75_sentence(ibuf);
+		} 
 	}
 }
 
@@ -125,14 +173,15 @@ vpl_parse_75_sentence(const char *ibuf)
 	gbint32 lat_raw, lon_raw;
 	gbint16 alt, speed_raw;
 	gbuint16 hdg_raw;
-	gbuint8 sats;
+	gbuint8 sats, hdop_raw, vdop_raw;
 	waypoint *waypt;
 	struct tm tm;
 
 	// The files have DOS line endings (CR/LF) but we don't care, because we
 	// don't read to the end.
-	sscanf(ibuf, "75%*2c%8X%8X%4hX%4hX%4hX%*2c%2hhX%*4c%6u%6u",
-		&lat_raw, &lon_raw, &alt, &speed_raw, &hdg_raw, &sats, &ymd, &hms);
+	sscanf(ibuf, "75%*2c%8X%8X%4hX%4hX%4hX%*2c%2hhX%2hhX%2hhX%6u%6u",
+		&lat_raw, &lon_raw, &alt, &speed_raw, &hdg_raw, &sats, &hdop_raw, &vdop_raw,
+		&ymd, &hms);
 
 	tm.tm_sec = hms % 100;
 	hms /= 100;
@@ -157,6 +206,8 @@ vpl_parse_75_sentence(const char *ibuf)
 	// Speed comes in (MPH x 0x10) which we have to convert to m/s
 	WAYPT_SET(waypt, speed, (speed_raw / (double) 0x10) * 0.44704);
 	waypt->course    = hdg_raw * (double) (360/65535);
+	waypt->hdop      = hdop_raw / (double) 8;
+	waypt->vdop      = vdop_raw / (double) 8;
 
 	waypt->creation_time = mkgmtime(&tm);
 
