9d3337c4702965d7b14435ae5725bf88154e4a2f
robertl <robertl@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2010-05-18 17:39:43 +0000

    Bump version number for repackage.

--- gpsbabel/gbversion.h
+++ gpsbabel/gbversion.h
@@ -4,5 +4,5 @@
  *
  * Isn't simplification via automation grand?
  */
-#define VERSION "1.3.7-beta20100516"
+#define VERSION "1.3.7-beta20100518"
 #define WEB_DOC_DIR "http://www.gpsbabel.org/htmldoc-development"
