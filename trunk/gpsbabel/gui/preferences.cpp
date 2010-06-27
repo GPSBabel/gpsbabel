a54124c33825e24b5294b5d858860bf7f20a72b3
robertl <robertl@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2010-06-27 18:28:16 +0000

    Tweak version check for 1.4.0

--- trunk/gpsbabel/gui/preferences.cpp
+++ trunk/gpsbabel/gui/preferences.cpp
@@ -41,6 +41,9 @@ Preferences::Preferences(QWidget* parent, QList<Format>& formatList,
   ui_.startupCheck->setChecked(bd_.startupVersionCheck);
   ui_.reportStatisticsCheck->setChecked(bd_.reportStatistics);
   ui_.ignoreVersionMismatchCheck->setChecked(bd_.ignoreVersionMismatch);
+  // Because of an unfortunate bug in 1.4.0, we turn this off in 1.4.1.
+  if (VERSION == "1.4.1") 
+    bd_.ignoreVersionMismatch = false;
 
   connect (ui_.buttonBox, SIGNAL(accepted()), this, SLOT(acceptClicked()));
   connect (ui_.buttonBox, SIGNAL(rejected()), this, SLOT(rejectClicked()));
