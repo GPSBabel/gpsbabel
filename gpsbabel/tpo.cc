ebacde351fc58378ea795b4208e361cf812d7431
robertlipe <robertlipe@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2013-05-01 02:48:36 +0000

    Fix scaler in tpo.cc reader.  Regenerate reference files.

--- gpsbabel/tpo.cc
+++ gpsbabel/tpo.cc
@@ -792,7 +792,7 @@ void tpo_process_tracks(void)
 
       // Process the delta
       else {
-        int scarray[] = {0,1,2,3,4,5,6,7,0,-7,-6,-5,-4,-3,-2,-1};
+        int scarray[] = {0,1,2,3,4,5,6,7,-8,-7,-6,-5,-4,-3,-2,-1};
 
 
         if (buf[jj] == 0) {
