569c38cdb708c3c56787c1cfcd18a3e01c691e11
robertlipe <robertlipe@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2013-07-18 03:10:00 +0000

    Avoid implicit time_t conversion in waypt_add that would result in creation_time being zeroed.

--- gpsbabel/waypt.cc
+++ gpsbabel/waypt.cc
@@ -158,7 +158,7 @@ waypt_add(waypoint *wpt)
   if ((wpt->longitude < -180) || (wpt->longitude > 180.0))
     fatal("Invalid longitude %f in waypoint %s.\n",
           lon_orig, wpt->shortname ? wpt->shortname : "");
-  if (wpt->creation_time < 0) {
+  if (!wpt->creation_time.isValid()) {
     warning("%s: Invalid timestamp in waypoint %s.\n",
             wpt->session->name,
             wpt->shortname ? wpt->shortname : "");
