0676a69ad01ba998e03f809698658b82c7cf214f
robertlipe@gmail.com <robertlipe@gmail.com@f51c46e8-681c-474f-0cfe-069cfd0219fb>
2013-12-31 19:43:08 +0000

    Compile fix for Ubuntu. Fix valgrind warning from scribble code.

--- trunk/gpsbabel/google.cc
+++ trunk/gpsbabel/google.cc
@@ -255,10 +255,10 @@ void goog_poly_e(xg_string args, const QXmlStreamAttributes* unused)
 // NEW_STRINGS FIXME(robertlipe): this is broken somehow there should be no need
 // to overallocate like this, but it's needed ot get an1 to not scribble
 // on itself.
-      wpt_tmp->shortname = (char*) xmalloc(7000);
 #if NEW_STRINGS
       wpt_tmp->shortname = QString().sprintf( "\\%5.5x", serial++);
 #else
+      wpt_tmp->shortname = (char*) xmalloc(7);
       sprintf(wpt_tmp->shortname, "\\%5.5x", serial++);
 #endif
       route_add_wpt(routehead[goog_segroute], wpt_tmp);
