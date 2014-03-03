2ef1b71cddbd1acedcef93a750b106bc95d18dbc
robertlipe <robertlipe@gmail.com>
2014-03-03 00:01:13 +0000

    Bump version.  Attemps to fix deployment issues on Windows.

--- gpsbabel/gbversion.h
+++ gpsbabel/gbversion.h
@@ -4,5 +4,5 @@
  *
  * Isn't simplification via automation grand?
  */
-#define VERSION "1.5.0-beta20140214"
+#define VERSION "1.5.0-beta20140302"
 #define WEB_DOC_DIR "http://www.gpsbabel.org/htmldoc-development"
