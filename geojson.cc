835b7198881ccfe6e7beed9f7392dcb60c9c0ab0
Harel Mazor <harel.mazor@gmail.com>
2017-01-25 23:49:57 +0200

    Updated documentation, added geojson.geojson test file, updated vecs array according to code review.

--- geojson.cc
+++ geojson.cc
@@ -313,7 +313,11 @@ geojson_write(void) {
 
 ff_vecs_t geojson_vecs = {
   ff_type_file,
-  { (ff_cap)(/*ff_cap_read | */ff_cap_write), ff_cap_write, ff_cap_none },
+  { 
+  	(ff_cap)(ff_cap_read | ff_cap_write) /* waypoints */,
+	(ff_cap)(ff_cap_read | ff_cap_write) /* tracks */,
+	(ff_cap)(ff_cap_read | ff_cap_write) /* routes */, 
+  },
   geojson_rd_init,
   geojson_wr_init,
   geojson_rd_deinit,
