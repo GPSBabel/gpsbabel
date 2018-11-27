ceb82a4b60f7dce73d4f96fa84ec91d31efeab99
tsteven4 <tsteven4@users.noreply.github.com>
2018-11-27 16:37:49 -0700

    simplify conversion from utm. (#275)
    * simplify conversion from utm.
    
    add test cases for Norway and Svalbard exceptions.
    
    * Correct bug in computing latitude band X.
    
    enhance test to cover exceptional width of band X.

--- jeeps/gpsmath.cc
+++ jeeps/gpsmath.cc
@@ -2004,19 +2004,19 @@ static int32 GPS_Math_LatLon_To_UTM_Param(double lat, double lon, int32* zone,
 {
   int32 ilon;
   int32 ilat;
-  int32 psign;
-  int32 lsign;
+  bool psign;
+  bool lsign;
 
   if (lat >= 84.0 || lat < -80.0) {
     return 0;
   }
 
-  psign = lsign = 0;
+  psign = lsign = false;
   if (lon < 0.0) {
-    lsign=1;
+    lsign=true;
   }
   if (lat < 0.0) {
-    psign=1;
+    psign=true;
   }
 
   ilon = abs((int32)lon);
@@ -2024,10 +2024,10 @@ static int32 GPS_Math_LatLon_To_UTM_Param(double lat, double lon, int32* zone,
 
   if (!lsign) {
     *zone = 31 + (ilon / 6);
-    *Mc   = (double)((ilon / 6) * 6 + 3);
+    *Mc   = static_cast<double>((ilon / 6) * 6 + 3);
   } else {
     *zone = 30 - (ilon / 6);
-    *Mc   = -(double)((ilon / 6) * 6 + 3);
+    *Mc   = -static_cast<double>((ilon / 6) * 6 + 3);
   }
 
   if (!psign) {
@@ -2042,14 +2042,19 @@ static int32 GPS_Math_LatLon_To_UTM_Param(double lat, double lon, int32* zone,
     }
   }
 
+  // The northernmost latitude band, X, is 12 degrees tall
+  // instead of the usual 8 degrees, i.e. [72.0,84.0).
+  if (*zc=='Y') {
+    *zc = 'X';
+  }
 
-  if (lat>=56.0 && lat<64.0 && lon>=3.0 &&
-      lon<12.0) {
+  // Norway exception
+  if (*zc=='V' && lon>=3.0 && lon<12.0) {
     *zone = 32;
-    *zc   = 'V';
     *Mc   = 9.0;
   }
 
+  // Svalbard exception
   if (*zc=='X' && lon>=0.0 && lon<42.0) {
     if (lon<9.0) {
       *zone = 31;
@@ -2169,32 +2174,14 @@ static int32 GPS_Math_UTM_Param_To_Mc(int32 zone, char zc, double* Mc,
                                       double* E0, double* N0, double* F0)
 {
 
-  if (zone>60 || zone<0 || zc<'C' || zc>'X') {
+  if (zone>60 || zone<0 || zc<'C' || zc>'X' || zc=='I' || zc=='O') {
     return 0;
   }
 
-  if (zone > 30) {
-    *Mc = (double)((zone-31)*6) + 3.0;
-  } else {
-    *Mc = (double) -(((30-zone)*6)+3);
-  }
+  *Mc = static_cast<double>(((zone-31)*6) + 3);
 
-  if (zone==32 && zc=='V') {
-    *Mc = 9.0;
-  }
-
-  if (zone==31 && zc=='X') {
-    *Mc = 3.0;
-  }
-  if (zone==33 && zc=='X') {
-    *Mc = 15.0;
-  }
-  if (zone==35 && zc=='X') {
-    *Mc = 27.0;
-  }
-  if (zone==37 && zc=='X') {
-    *Mc = 39.0;
-  }
+  // The Norway and Svalbard exceptions do NOT change the central meridian of
+  // the impacted zones (31V, 32V, 31X, 33X, 35X, 37X).
 
   if (zc>'M') {
     *N0 = 0.0;
