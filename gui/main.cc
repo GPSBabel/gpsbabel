1250fddc619243b6564ffc8b4117f58d4b833a50
tsteven4 <tsteven4@users.noreply.github.com>
2019-02-16 13:41:25 -0700

    Set Qt version floor at 5.9 (#304)
    * move Qt floor to 5.9.
    
    * update coverage build for Qt archive naming.
    
    * update all travis builds to xenial.
    
    trusty is EOL 4/2019.
    
    * avoid 5.12.1 on linux CI.
    
    * Revert "avoid 5.12.1 on linux CI."
    
    This reverts commit b86212123cda9d7c57f212f3baf0f05bc3121ee5.
    
    * avoid 5.12.1 on linux CI.

--- gui/main.cc
+++ gui/main.cc
@@ -45,7 +45,7 @@ const char *pathSeparator = ":";
 int main(int argc, char**argv)
 {
 // MIN_QT_VERSION in configure.ac should correspond to the QT_VERSION_CHECK arguments in main.cc and gui/main.cc
-#if (QT_VERSION < QT_VERSION_CHECK(5, 7, 0))
+#if (QT_VERSION < QT_VERSION_CHECK(5, 9, 0))
   #error this version of Qt is not supported.
 #endif
 
