340fffd6baf2bbd1661f16c84092870afd7e19df
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2020-01-26 08:26:58 -0700

    refactor height filter to appease cppcheck & MSVC (#473)
    * refactor heightgrid to appease cppcheck.
    
    With newer versions (somwhere after 1.82) of cppcheck analyzing
    height.cc took an excessive amount of time.
    By making these variables private static class members instead of
    member function static variables we avoid the cppcheck issues.
    
    * tweak height to work around MSVC 2017 error.
    
    and hopefully MSVC 2015 error as well.
    
    * don't include cstdint within class definition.
    
    It can lead to compile failures.  This was done in heightgrid.h,
    but the include guards in cstdint hid the error.

--- height.h
+++ height.h
@@ -24,10 +24,12 @@
 #ifndef HEIGHT_H_INCLUDED_
 #define HEIGHT_H_INCLUDED_
 
+#include <cstdint>         // for int8_t in heightgrid.h
+
 #include <QtCore/QVector>  // for QVector
 
-#include "defs.h"    // for ARG_NOMINMAX, Waypoint (ptr only), arglist_t
-#include "filter.h"  // for Filter
+#include "defs.h"          // for arglist_t, ARG_NOMINMAX, ARGTYPE_BEGIN_REQ, ARGTYPE_BOOL, ARGTYPE_END_REQ, ARGTYPE_FLOAT, Waypoint
+#include "filter.h"        // for Filter
 
 #if FILTERS_ENABLED
 
@@ -45,6 +47,8 @@ private:
   char* addopt        = nullptr;
   char* wgs84tomslopt = nullptr;
   double addf;
+  // include static constexpr data member definitions with intializers for grid as private members.
+  #include "heightgrid.h"
 
   QVector<arglist_t> args = {
     {
