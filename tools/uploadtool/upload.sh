b95ed3d13d05a38df866636a2049b003d90b6de3
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2020-03-27 16:29:14 -0600

    migrate to travis-ci.com (#526)
    * get qt installs working to load cache.
    
    * update to travis-ci.com for github release build log links.

--- tools/uploadtool/upload.sh
+++ tools/uploadtool/upload.sh
@@ -156,7 +156,7 @@ if [ "$TRAVIS_COMMIT" != "$target_commit_sha" ] ; then
   if [ ! -z "$TRAVIS_JOB_ID" ] ; then
     if [ -z "${UPLOADTOOL_BODY+x}" ] ; then
       # TODO: The host could be travis-ci.org (legacy open source) or travis-ci.com (subscription or latest open source).
-      BODY="Travis CI build log: https://travis-ci.org/$REPO_SLUG/builds/$TRAVIS_BUILD_ID/"
+      BODY="Travis CI build log: https://travis-ci.com/$REPO_SLUG/builds/$TRAVIS_BUILD_ID/"
     else
       BODY="$UPLOADTOOL_BODY"
     fi
