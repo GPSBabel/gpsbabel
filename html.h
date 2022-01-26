e55a0d13e21da508776db6c66dc54677b1bbd192
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2022-01-26 10:12:27 -0700

    convert html to Format class.

---
+++ html.h
@@ -0,0 +1,105 @@
+/*
+    Output only format for Human Readable formats.
+
+    Copyright (C) 2004 Scott Brynen, scott (at) brynen.com
+    Copyright (C) 2002 Robert Lipe, robertlipe+source@gpsbabel.org
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+#ifndef HTML_H_INCLUDED_
+#define HTML_H_INCLUDED_
+
+#include <QString>   // for QString
+#include <QVector>   // for QVector
+
+#include "defs.h"    // for arglist_t, ff_cap, ARG_NOMINMAX, ARGTYPE_STRING, ARGTYPE_BOOL, Waypoint, ff_cap_none, CET_CHARSET_UTF8, ff_cap_write, ff_type, ff_type_file, short_handle
+#include "format.h"  // for Format
+#include "gbfile.h"  // for gbfile
+
+
+class HtmlFormat : public Format
+{
+public:
+  QVector<arglist_t>* get_args() override
+  {
+    return &html_args;
+  }
+
+  ff_type get_type() const override
+  {
+    return ff_type_file;
+  }
+
+  QVector<ff_cap> get_cap() const override
+  {
+    return { ff_cap_write, ff_cap_none, ff_cap_none };
+  }
+
+  QString get_encode() const override
+  {
+    return CET_CHARSET_UTF8;
+  }
+
+  int get_fixed_encode() const override
+  {
+    return 0;
+  }
+
+  void wr_init(const QString& fname) override;
+  void write() override;
+  void wr_deinit() override;
+
+private:
+  /* Member Functions */
+
+  void html_disp(const Waypoint* wpt) const;
+  void html_index(const Waypoint* wpt) const;
+
+  /* Data Members */
+
+  gbfile* file_out;
+  short_handle mkshort_handle;
+
+  char* stylesheet = nullptr;
+  char* html_encrypt = nullptr;
+  char* includelogs = nullptr;
+  char* degformat = nullptr;
+  char* altunits = nullptr;
+
+  QVector<arglist_t> html_args = {
+    {
+      "stylesheet", &stylesheet,
+      "Path to HTML style sheet", nullptr, ARGTYPE_STRING, ARG_NOMINMAX, nullptr
+    },
+    {
+      "encrypt", &html_encrypt,
+      "Encrypt hints using ROT13", nullptr, ARGTYPE_BOOL, ARG_NOMINMAX, nullptr
+    },
+    {
+      "logs", &includelogs,
+      "Include groundspeak logs if present", nullptr, ARGTYPE_BOOL, ARG_NOMINMAX, nullptr
+    },
+    {
+      "degformat", &degformat,
+      "Degrees output as 'ddd', 'dmm'(default) or 'dms'", "dmm", ARGTYPE_STRING, ARG_NOMINMAX, nullptr
+    },
+    {
+      "altunits", &altunits,
+      "Units for altitude (f)eet or (m)etres", "m", ARGTYPE_STRING, ARG_NOMINMAX, nullptr
+    },
+  };
+
+};
+#endif // HTML_H_INCLUDED_
--- vecs.h
+++ vecs.h
@@ -36,6 +36,7 @@
 #include "ggv_bin.h"
 #include "globalsat_sport.h"
 #include "gpx.h"
+#include "html.h"
 #include "kml.h"
 #include "legacyformat.h"
 #include "lowranceusr.h"
@@ -65,7 +66,6 @@ extern ff_vecs_t easygps_vecs;
 extern ff_vecs_t saroute_vecs;
 extern ff_vecs_t gpl_vecs;
 extern ff_vecs_t text_vecs;
-extern ff_vecs_t html_vecs;
 extern ff_vecs_t igc_vecs;
 extern ff_vecs_t brauniger_iq_vecs;
 extern ff_vecs_t mtk_vecs;
@@ -253,7 +253,7 @@ private:
 #endif
   LegacyFormat gpl_fmt {gpl_vecs};
   LegacyFormat text_fmt {text_vecs};
-  LegacyFormat html_fmt {html_vecs};
+  HtmlFormat html_fmt;
   LegacyFormat igc_fmt {igc_vecs};
   LegacyFormat brauniger_iq_fmt {brauniger_iq_vecs};
   LegacyFormat mtk_fmt {mtk_vecs};
