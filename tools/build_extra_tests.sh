950f55ffe70bdb921dfbfc2361ad5bbea12c813b
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2022-10-03 11:53:08 -0600

    switch linux CI extra_tests and coverage builds to cmake.

--- tools/build_extra_tests.sh
+++ tools/build_extra_tests.sh
@@ -4,6 +4,8 @@
 # output is conditionally mailed to gpsbabel-code.
 #
 
+SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )/.." && pwd)"
+
 # echo some system info to log
 uname -a
 if [ -e /etc/system-release ]; then
@@ -12,32 +14,42 @@ fi
 if [ -e /etc/os-release ]; then
 	cat /etc/os-release
 fi
-git --no-pager log -n 1
+git -C "${SOURCE_DIR}" --no-pager log -n 1
 
 # build and test keeping output within the pwd.
-export GBTEMP=$(mktemp -d -p $(pwd) GBTEMPXXXX)
+GBTEMP=$(mktemp -d -p "$(pwd)" GBTEMPXXXX)
+export GBTEMP
 
+rm -rf bld-sanitizeaddress
+mkdir bld-sanitizeaddress
+pushd bld-sanitizeaddress
 #note that debug will also enable assertions.
-qmake "CONFIG+=debug sanitizer sanitize_address"
-make clean
-make -j 3
-make check
+cmake -DCMAKE_BUILD_TYPE=Debug -DGPSBABEL_EXTRA_COMPILE_OPTIONS="-fsanitize=address -fno-omit-frame-pointer" -DGPSBABEL_EXTRA_LINK_OPTIONS="-fsanitize=address" -G Ninja "${SOURCE_DIR}"
+cmake --build . --target gpsbabel --verbose
+cmake --build . --target check
+popd
 
-qmake "CONFIG+=debug sanitizer sanitize_undefined"
-make clean
-make -j 3
-make check
+rm -rf bld-sanitizeundefined
+mkdir bld-sanitizeundefined
+pushd bld-sanitizeundefined
+cmake -DCMAKE_BUILD_TYPE=Debug -DGPSBABEL_EXTRA_COMPILE_OPTIONS="-fsanitize=undefined -fsanitize=float-divide-by-zero -fno-omit-frame-pointer" -DGPSBABEL_EXTRA_LINK_OPTIONS="-fsanitize=undefined -fsanitize=float-divide-by-zero" -G Ninja "${SOURCE_DIR}"
+cmake --build . --target gpsbabel --verbose
+cmake --build . --target check
+popd
+
+rm -rf "${GBTEMP}"
 
 # run clazy on both gpsbabel and gpsbabelfe.
 # unlike qmake, cmake uses system includes for Qt which quiets warnings
 # from the Qt headers.
+rm -rf bld-clazy
+mkdir bld-clazy
+pushd bld-clazy
 export CLAZY_CHECKS=level0,level1,no-non-pod-global-static,no-qstring-ref
-cmake . -DCMAKE_CXX_COMPILER=clazy -G "Ninja" -DCMAKE_BUILD_TYPE:STRING="Debug"
-cmake --build . --target clean
+cmake -DCMAKE_CXX_COMPILER=clazy -DCMAKE_BUILD_TYPE=Debug -G Ninja "${SOURCE_DIR}"
 cmake --build . 2>&1 | tee clazy.log
 if grep -- '-Wclazy' clazy.log; then
   exit 1
 else
   exit 0
 fi
-
