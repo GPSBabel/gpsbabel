6b3d7335cfca53690eee56a8bd13f6b1f816b91f
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2022-10-25 10:27:49 -0600

    convert to docbook 5.0.

--- tools/make_gpsbabel_html.sh
+++ tools/make_gpsbabel_html.sh
@@ -9,5 +9,5 @@ xsltproc \
   --stringparam make.clean.html "1" \
   --stringparam html.valid.html "1" \
   --stringparam html.stylesheet "https://www.gpsbabel.org/style3.css" \
-  http://docbook.sourceforge.net/release/xsl/current/xhtml/docbook.xsl \
+  http://docbook.sourceforge.net/release/xsl-ns/current/xhtml/docbook.xsl \
   xmldoc/readme.xml
--- tools/make_gpsbabel_pdf.sh
+++ tools/make_gpsbabel_pdf.sh
@@ -2,6 +2,6 @@
 set -ex
 
 perl xmldoc/makedoc
-xmllint --noout --valid xmldoc/readme.xml #valid and well-formed
+jing /usr/share/xml/docbook/schema/rng/5.0/docbook.rng xmldoc/readme.xml
 xsltproc -o gpsbabel.fo xmldoc/babelpdf.xsl xmldoc/readme.xml
 HOME=. fop -q -fo gpsbabel.fo -pdf gpsbabel.pdf
