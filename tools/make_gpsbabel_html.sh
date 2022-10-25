871fececf47be573be91d0765fb75dae8fd76689
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2022-10-25 13:26:07 -0600

    use uri to find docbook rng.

--- tools/make_gpsbabel_html.sh
+++ tools/make_gpsbabel_html.sh
@@ -2,12 +2,7 @@
 set -ex
 
 perl xmldoc/makedoc
-if [ "$(uname -s)" = "Darwin" ]; then
-  # assume jing and docbook rng provided by homebrew
-  /usr/local/bin/jing /usr/local/opt/docbook/docbook/xml/5.0/rng/docbook.rng xmldoc/readme.xml
-else
-  jing /usr/share/xml/docbook/schema/rng/5.0/docbook.rng xmldoc/readme.xml
-fi
+jing https://docbook.org/xml/5.0/rng/docbook.rng xmldoc/readme.xml
 xsltproc \
   --output gpsbabel.html \
   --stringparam toc.section.depth "1" \
--- tools/make_gpsbabel_pdf.sh
+++ tools/make_gpsbabel_pdf.sh
@@ -2,11 +2,6 @@
 set -ex
 
 perl xmldoc/makedoc
-if [ "$(uname -s)" = "Darwin" ]; then
-  # assume jing and docbook rng provided by homebrew
-  /usr/local/bin/jing /usr/local/opt/docbook/docbook/xml/5.0/rng/docbook.rng xmldoc/readme.xml
-else
-  jing /usr/share/xml/docbook/schema/rng/5.0/docbook.rng xmldoc/readme.xml
-fi
+jing https://docbook.org/xml/5.0/rng/docbook.rng xmldoc/readme.xml
 xsltproc -o gpsbabel.fo xmldoc/babelpdf.xsl xmldoc/readme.xml
 HOME=. fop -q -fo gpsbabel.fo -pdf gpsbabel.pdf
