8f8f03f46e2928ba152cd038eaebcb6d7ba872ab
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2022-11-24 12:02:54 -0700

    try to find common docbook url for fedora/ubuntu/homebrew catalogs.

--- tools/make_gpsbabel_doc.sh
+++ tools/make_gpsbabel_doc.sh
@@ -2,9 +2,9 @@
 set -ex
 
 perl xmldoc/makedoc
-xmllint --noout --relaxng http://www.oasis-open.org/docbook/xml/5.0/rng/docbook.rng xmldoc/readme.xml
+xmllint --noout --relaxng http://docbook.org/xml/5.0/rng/docbook.rng xmldoc/readme.xml
 # the following doesn't seem to work.
-#xmllint --noout --schematron http://www.oasis-open.org/docbook/xml/5.0/sch/docbook.sch xmldoc/readme.xml
+#xmllint --noout --schematron http://docbook.org/xml/5.0/sch/docbook.sch xmldoc/readme.xml
 # jing and many depedencies removed from fedora
 if command -v jing >/dev/null 2>&1; then
   jing http://docs.oasis-open.org/docbook/xml/5.0/rng/docbook.rng xmldoc/readme.xml
