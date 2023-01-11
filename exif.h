f7b5de7e920093b08921d4fff58941e4d853998d
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2023-01-11 11:49:06 -0700

    eliminate some uses of xstrdup/xstrndup. (#977)
    and fix another related memory leak!

--- exif.h
+++ exif.h
@@ -156,7 +156,7 @@ private:
   static void print_buff(const char* buf, int sz, const char* cmt);
   static uint16_t exif_type_size(uint16_t type);
   static QString exif_time_str(const QDateTime& time);
-  static char* exif_read_str(ExifTag* tag);
+  static QByteArray exif_read_str(ExifTag* tag);
   static double exif_read_double(const ExifTag* tag, int index);
   static double exif_read_coord(const ExifTag* tag);
   static QTime exif_read_timestamp(const ExifTag* tag);
