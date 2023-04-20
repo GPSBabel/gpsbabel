8ca08ae2b5fba83eab1694de3c48b17e46aba07f
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2023-04-20 13:21:41 -0600

    exercise qt 6.5.0 on macos CI (#1073)

--- tools/ci_script_osx.sh
+++ tools/ci_script_osx.sh
@@ -3,7 +3,7 @@
 # this script is run on ci for the script stage of mac builds
 #
  
-function version_ge() { test "$(printf "%s\n%s" $1 $2 | sort -rV | head -n 1)" == "$1"; }
+function version_ge() { test "$(printf "%s\n%s" "$1" "$2" | sort -rV | head -n 1)" == "$1"; }
 
 if [ $# -lt 2 ]; then
   echo "Usage: $0 source_directory qt_version [Generator]"
@@ -17,7 +17,10 @@ if [ $# -ge 3 ]; then
     GENERATOR[1]=$3
   fi
 fi
-if version_ge "${QTVER}" 6.0.0; then
+if version_ge "${QTVER}" 6.5.0; then
+  DEPLOY_TARGET="11.0"
+  ARCHS="x86_64;arm64"
+elif version_ge "${QTVER}" 6.0.0; then
   DEPLOY_TARGET="10.14"
   ARCHS="x86_64;arm64"
 elif version_ge "${QTVER}" 5.14.0; then
@@ -29,7 +32,7 @@ else
 fi
   
 # we assume we are on macOS, so date is not gnu date.
-VERSIONID=${VERSIONID:-$(date -ju -f %Y-%m-%dT%H:%M:%S%z $(git show -s --format="%aI" HEAD | sed 's/:\(..\)$/\1/') +%Y%m%dT%H%MZ)-$(git rev-parse --short=7 HEAD)}
+VERSIONID=${VERSIONID:-$(date -ju -f %Y-%m-%dT%H:%M:%S%z "$(git show -s --format="%aI" HEAD | sed 's/:\(..\)$/\1/')" +%Y%m%dT%H%MZ)-$(git rev-parse --short=7 HEAD)}
 
 # debug tokens
 "$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"/ci_tokens
