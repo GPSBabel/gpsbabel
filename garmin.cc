9c1e1af2e35dc5a62db840081773a036e8e95ed2
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2023-05-15 17:40:44 -0600

    correct assertion

--- garmin.cc
+++ garmin.cc
@@ -387,7 +387,7 @@ rw_init(const QString& fname)
   assert(!QString(valid_waypt_chars).contains('['));
   assert(!QString(valid_waypt_chars).contains(']'));
   invalid_char_re = QRegularExpression(QStringLiteral("[^%1]").arg(valid_waypt_chars));
-  assert(!invalid_char_re.isValid());
+  assert(invalid_char_re.isValid());
 }
 
 static void
