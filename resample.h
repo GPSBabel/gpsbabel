4386f99626e02b89fecc30857aab38a6addd1230
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2023-08-23 10:32:23 -0600

    resolve maintanence issue in interpolate & resample filters. (#1161)
    * resolve maintanence issue in interpolate filter.
    
    Instead of manually creating a deep copy of the route list with
    an empty waypoint list we operate on the original route list by
    swapping it's waypoint list with an empty list.  This is both more
    efficient and easier to maintain.
    
    * resolve maintanence issues in resample filter.
    
    Instead of manually creating a deep copy of the route list with
    an empty waypoint list we operate on the original route list by
    swapping it's waypoint list with an empty list.  This is both more
    efficient and easier to maintain.
    
    For efficient deletion use track_del_marked_wpts.

--- resample.h
+++ resample.h
@@ -1,7 +1,7 @@
 /*
     Track resampling filter
 
-    Copyright (C) 2021 Robert Lipe, robertlipe+source@gpsbabel.org
+    Copyright (C) 2021,2023 Robert Lipe, robertlipe+source@gpsbabel.org
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -24,6 +24,7 @@
 
 #include <tuple>               // for tuple
 
+#include <QString>             // for QString
 #include <QVector>             // for QVector
 
 #include "defs.h"              // for arglist_t, ARGTYPE_INT, Waypoint, route_head
@@ -46,7 +47,13 @@ public:
 
 private:
 
+  /* Member Functions */
+
   void average_waypoint(Waypoint* wpt, bool zero_stuffed);
+  void interpolate_rte(route_head* rte);
+  void decimate_rte(const route_head* rte);
+
+  /* Data Members */
 
   QVector<std::tuple<gpsbabel::NVector, int, double>> history;
   gpsbabel::NVector accumulated_position;
