51a5ce3d9c4d3699c8a51f64655588709cb1fac2
PacketFiend <kenneth@voort.ca>
2023-10-14 16:06:41 -0400

    Make QT5 happy

--- igc.h
+++ igc.h
@@ -81,6 +81,13 @@ public:
     ext_rec_acz = 10,  // Z Acceleration
   };
 
+  // Qt5 doesn't have a qHash function for scoped enumerations.
+  // Qt6 falls back to std::hash, but it may not use the seed.
+  friend qhash_result_t qHash(const igc_ext_type_t& key, qhash_result_t seed = 0) noexcept
+  {
+    return qHash(static_cast<std::underlying_type<igc_ext_type_t>::type>(key), seed);
+  }
+
   QVector<arglist_t>* get_args() override
   {
     return &igc_args;
