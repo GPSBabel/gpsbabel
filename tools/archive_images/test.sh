5a510c79bb4d5aaba028241d8145e10aea828002
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2024-09-09 15:25:47 -0600

    enhance test script to avoid cwd dependencies.

--- tools/archive_images/test.sh
+++ tools/archive_images/test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -x
-./tools/archive_images/run_gpsbabel.sh -v 1.5.0 -- -D1
-./tools/archive_images/run_gpsbabel.sh -v 1.5.1 -- -D1
-./tools/archive_images/run_gpsbabel.sh -v 1.5.2 -- -D1
-./tools/archive_images/run_gpsbabel.sh -v 1.5.3 -- -D1
-./tools/archive_images/run_gpsbabel.sh -v 1.5.4 -- -D1
-./tools/archive_images/run_gpsbabel.sh -v 1.6.0 -- -D1
-./tools/archive_images/run_gpsbabel.sh -v 1.7.0 -- -D1
-./tools/archive_images/run_gpsbabel.sh -v 1.8.0 -- -D1
-./tools/archive_images/run_gpsbabel.sh -v 1.9.0 -- -D1
+SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
+
+"${SOURCE_DIR}/run_gpsbabel.sh" -v 1.5.0 -- -D1
+"${SOURCE_DIR}/run_gpsbabel.sh" -v 1.5.1 -- -D1
+"${SOURCE_DIR}/run_gpsbabel.sh" -v 1.5.2 -- -D1
+"${SOURCE_DIR}/run_gpsbabel.sh" -v 1.5.3 -- -D1
+"${SOURCE_DIR}/run_gpsbabel.sh" -v 1.5.4 -- -D1
+"${SOURCE_DIR}/run_gpsbabel.sh" -v 1.6.0 -- -D1
+"${SOURCE_DIR}/run_gpsbabel.sh" -v 1.7.0 -- -D1
+"${SOURCE_DIR}/run_gpsbabel.sh" -v 1.8.0 -- -D1
+"${SOURCE_DIR}/run_gpsbabel.sh" -v 1.9.0 -- -D1
