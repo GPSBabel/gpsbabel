0c2047b6e0c986acbb9d3c0eb23e30558ba85ac3
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2024-09-09 17:44:39 -0600

    make sure to clean up container

--- tools/archive_images/run_gpsbabel.sh
+++ tools/archive_images/run_gpsbabel.sh
@@ -8,9 +8,9 @@ done
 shift $(($OPTIND -1))
 
 container=$(docker create -q -i -t -w /app -v "$(pwd):/app" "tsteven4/gpsbabel:${VERSION}")
+trap "docker rm -f ${container} >/dev/null" 0 1 2 3 15
 docker start "${container}" >/dev/null
 docker exec -i -t "${container}" setup_user.sh "$(id -u)" "$(id -g)"
 docker exec -i -t -u "$(id -u):$(id -g)" "${container}" gpsbabel "$@"
-docker rm -f "${container}" >/dev/null
 
 
