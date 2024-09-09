145e644d2ffa4dae06238929c59d246bda681b10
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2024-09-09 15:16:50 -0600

    add scripts to automate image creation

---
+++ tools/archive_images/push.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -ex
+docker push tsteven4/gpsbabel:1.5.0
+docker push tsteven4/gpsbabel:1.5.1
+docker push tsteven4/gpsbabel:1.5.2
+docker push tsteven4/gpsbabel:1.5.3
+docker push tsteven4/gpsbabel:1.5.4
+docker push tsteven4/gpsbabel:1.6.0
+docker push tsteven4/gpsbabel:1.7.0
+docker push tsteven4/gpsbabel:1.8.0
+docker push tsteven4/gpsbabel:1.9.0
---
+++ tools/archive_images/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+./tools/archive_images/run_gpsbabel.sh -v 1.5.0 -- -D1
+./tools/archive_images/run_gpsbabel.sh -v 1.5.1 -- -D1
+./tools/archive_images/run_gpsbabel.sh -v 1.5.2 -- -D1
+./tools/archive_images/run_gpsbabel.sh -v 1.5.3 -- -D1
+./tools/archive_images/run_gpsbabel.sh -v 1.5.4 -- -D1
+./tools/archive_images/run_gpsbabel.sh -v 1.6.0 -- -D1
+./tools/archive_images/run_gpsbabel.sh -v 1.7.0 -- -D1
+./tools/archive_images/run_gpsbabel.sh -v 1.8.0 -- -D1
+./tools/archive_images/run_gpsbabel.sh -v 1.9.0 -- -D1
