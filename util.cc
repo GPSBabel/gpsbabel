f5eeaebd5f72aceb36140714a41afc66e38b1b8b
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2024-09-15 08:19:46 -0600

    Revert "strip_html deletes other tags"
    This reverts commit 40fe2ef0153d8c2e6d53432b9184f8acffbab2ef.

--- util.cc
+++ util.cc
@@ -785,9 +785,6 @@ QString strip_html(const QString& utfstring)
   assert(trre.isValid());
   static const QRegularExpression tdre("<td.*?>", QRegularExpression::CaseInsensitiveOption | QRegularExpression::DotMatchesEverythingOption);
   assert(tdre.isValid());
-  static const QRegularExpression otherre("<.*?>", QRegularExpression::CaseInsensitiveOption | QRegularExpression::DotMatchesEverythingOption);
-  assert(otherre.isValid());
-
 
   QString out(utfstring);
 
@@ -797,7 +794,6 @@ QString strip_html(const QString& utfstring)
   out.replace(trre, "\n");
   out.replace(tdre, " ");
   out.replace("<img", "[IMG]", Qt::CaseInsensitive);
-  out.replace(otherre, "");
 
   // Then entity replacement (entities are case sensitive)
   out.replace("&amp;","&");
