dcec0521077ecf2101c0bbb49d5d3675d6cdb0c0
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2025-02-22 06:37:50 -0700

    avoid Qt seesion warning

--- snap/local/gpsbabelfe.sh
+++ snap/local/gpsbabelfe.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# avoid warning message:
+# Qt: Session management error: Could not open network socket
+export -n SESSION_MANAGER
+
 if [ -n "$XDG_RUNTIME_DIR" ]; then
   if [ -S "$XDG_RUNTIME_DIR/../${WAYLAND_DISPLAY:-wayland-0}" ]; then
     if [ ! -e "$XDG_RUNTIME_DIR/${WAYLAND_DISPLAY:-wayland-0}" ]; then
