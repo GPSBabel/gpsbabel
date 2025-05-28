49d88a120cb134d979ffcd276dfba8fda9d3a6c1
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2025-05-27 18:33:50 -0600

    enhance comments in launch script.

--- snap/local/gpsbabelfe.sh
+++ snap/local/gpsbabelfe.sh
@@ -2,11 +2,13 @@
 
 # avoid warning message:
 # Qt: Session management error: Could not open network socket
-# also can be fixed with viruscamp's wslg-runtime-dir.service.
-# Supposedly fixed in WSL 2.5.1.
+# This occurs with an xrdp session, e.g. Xorg.
 
 export -n SESSION_MANAGER
 
+# Avoid issues with WSL2 https://github.com/microsoft/WSL/issues/11261, https://github.com/microsoft/WSL/issues/12616
+# This can be fixed more generally with viruscamp's wslg-runtime-dir.service.
+# Supposedly fixed in WSL prerelease 2.5.1, release 2.5.7 has been tested.
 if [ -n "$XDG_RUNTIME_DIR" ]; then
   if [ -S "$XDG_RUNTIME_DIR/../${WAYLAND_DISPLAY:-wayland-0}" ]; then
     if [ ! -e "$XDG_RUNTIME_DIR/${WAYLAND_DISPLAY:-wayland-0}" ]; then
