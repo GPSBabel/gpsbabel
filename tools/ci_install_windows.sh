a01d57a78b7f7858937d8b3fd9e14dd5b657b9cf
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2025-10-07 08:23:44 -0600

    comment aqt windows arm64 workaround.

--- tools/ci_install_windows.sh
+++ tools/ci_install_windows.sh
@@ -60,6 +60,8 @@ else
   mkdir -p "${CACHEDIR}"
 
   if [ "${METHOD}" = "aqt" ]; then
+    # we need https://github.com/miurahr/aqtinstall/pull/941 to install extensions with 6.10.0 for windows arm64.
+    # until this is merged and released use a locally generated version of aqt.
     #pip3 install 'aqtinstall>=3.3.0'
     archive=aqtinstall-3.3.1.dev13-py3-none-any.whl
     curl -u "${ARTIFACTORY_USER}:${ARTIFACTORY_API_KEY}" "${ARTIFACTORY_BASE_URL}/${archive}" -o "/tmp/${archive}"
