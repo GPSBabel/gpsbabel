216004e2d800ab3729f7adfaeb43e8076c039e2a
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2026-05-17 09:09:01 -0700

    add generated assets

--- gui/make_car_file.sh
+++ gui/make_car_file.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -ex
 
-xcrun actool images/macos/AppIcon.icon --compile images/macos --app-icon AppIcon --platform macos --target-device mac --minimum-deployment-target 10.12 --include-all-app-icons --output-partial-info-plist /dev/null --output-format human-readable-text
+xcrun actool images/macos/AppIcon.icon --compile images/macos --app-icon AppIcon --platform macosx --target-device mac --minimum-deployment-target 10.12 --include-all-app-icons --output-partial-info-plist /dev/null --output-format human-readable-text
