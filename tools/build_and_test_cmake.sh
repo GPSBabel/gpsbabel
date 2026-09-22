57f5aa04ed13787f605c3730a2c10c7e9f281ce4
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2026-09-22 12:59:29 -0600

    Add ubuntu resolute (26.04) to CI. (#1634)

--- tools/build_and_test_cmake.sh
+++ tools/build_and_test_cmake.sh
@@ -3,6 +3,19 @@
 # this script is triggered by SCM changes and is run on the build server.
 # output is conditionally mailed to gpsbabel-code.
 #
+while getopts dD:tv name
+do
+  case $name in
+    d) DOCS=ON;;
+    D) DOCS=ON; DOCVERSION="$OPTARG";;
+    t) TESTALL=ON;;
+    v) VALGRIND=ON;;
+    ?) printf "Usage: %s: [-d] [-D docversion] [-t] [-v]\n" "$0"
+       exit 2;;
+  esac
+done
+shift "$((OPTIND - 1))"
+
 # echo some system info to log
 uname -a
 if [ -e /etc/system-release ]; then
@@ -13,10 +26,11 @@ if [ -e /etc/os-release ]; then
 fi
 git --no-pager log -n 1
 # build and test keeping output within the pwd.
-export GBTEMP=$(pwd)/gbtemp
+GBTEMP=$(pwd)/gbtemp
+export GBTEMP
 mkdir -p "$GBTEMP"
-if [ -n "$1" ]; then
-  cmake . -G Ninja -DCMAKE_BUILD_TYPE=Release -DGPSABEL_DOCVERSION="$1"
+if [ -n "$DOCVERSION" ]; then
+  cmake . -G Ninja -DCMAKE_BUILD_TYPE=Release -DGPSBABEL_DOCVERSION="$DOCVERSION"
 else
   cmake . -G Ninja -DCMAKE_BUILD_TYPE=Release
 fi
@@ -26,20 +40,26 @@ fi
 #make toolinfo
 cmake --build . --target clean
 cmake --build . --target gpsbabel
-cmake --build . --target gpsbabel.html
-cmake --build . --target gpsbabel.pdf
-cmake --build . --target gpsbabel.org
+if [ "${DOCS}" = "ON" ]; then
+  cmake --build . --target gpsbabel.html
+  cmake --build . --target gpsbabel.pdf
+  cmake --build . --target gpsbabel.org
+fi
 cmake --build . --target check
 cmake --build . --target gpsbabelfe
 # test for mangled encoding of command line arguments
 ./test_encoding_latin1
 ./test_encoding_utf8
 #make torture
-cmake --build . --target check-vtesto
-# eat the verbose output from test-all, including crash.output
-# this is a bit risky, if test-all generates an error we won't see what happened.
-echo "test-all in progress... (read/write test between all possible formats)"
-(LIBC_FATAL_STDERR_=1; export LIBC_FATAL_STDERR_; ./test-all -s -r reference/expertgps.gpx >/dev/null 2>&1)
-# summarize the test-all results, and generate an error if a fatal error was
-# detected by test-all.
-./test-all -J
+if [ "${VALGRIND}" = "ON" ]; then
+  cmake --build . --target check-vtesto
+fi
+if [ "${TESTALL}" = "ON" ]; then
+  # eat the verbose output from test-all, including crash.output
+  # this is a bit risky, if test-all generates an error we won't see what happened.
+  echo "test-all in progress... (read/write test between all possible formats)"
+  (LIBC_FATAL_STDERR_=1; export LIBC_FATAL_STDERR_; ./test-all -s -r reference/expertgps.gpx >/dev/null 2>&1)
+  # summarize the test-all results, and generate an error if a fatal error was
+  # detected by test-all.
+  ./test-all -J
+fi
